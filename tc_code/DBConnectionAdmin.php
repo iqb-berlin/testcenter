72ab643c50c5c2982de887948a678ae24e3b496f
andreistroescu <andrei.g.stroescu@gmail.com>
2018-08-21 17:41:24 +0200

    WIP writing helper functions to get data from both DB and XML

--- tc_code/DBConnectionAdmin.php
+++ tc_code/DBConnectionAdmin.php
@@ -234,5 +234,62 @@ class DBConnectionAdmin extends DBConnection {
         return $obj;
  
     }
+
+    public function doUsefulXMLChecking($wsId) {
+        if(is_numeric($wsId)) {
+          if($wsId > 0) {
+            $sanitizedwsId = intval($wsId);
+    
+            $TesttakersDirname = __DIR__.'/../tc_data/ws_' . $sanitizedwsId . '/Testtakers';
+            error_log($TesttakersDirname);
+            if (file_exists($TesttakersDirname)) {
+    
+              $testTakersDirectoryHandle = opendir($TesttakersDirname);
+    
+              // reading file by file, $filename stores the name of the next file in the directory
+              while (($filename = readdir($testTakersDirectoryHandle))) { 
+    
+                  // checking if files still exist ahead  
+                  if ($filename !== false) {             
+    
+                    $fullfilename = $TesttakersDirname . '/' . $filename; // complete file path
+                    
+                      // checking if there is a file at the full file path and if it is an .xml
+                      if (is_file($fullfilename) && (strtoupper(substr($filename, -4)) == '.XML')) {
+                        $xmlfile = simplexml_load_file($fullfilename);
+                        
+                        // if the xml file has loaded successfully into $xmlfile
+                        if ($xmlfile != false) {
+                           return true; 
+                        } else { 
+                            error_log('Error: There was no file found!');
+                        }
+                    } else { 
+                        error_log('Error: There might not be a file there or it is not of .xml format!');
+                    }
+                } else {
+                    break;
+                    // if there are no more files in the folder then exit the while loop
+                }
+            }
+            } else { 
+            error_log('Error: Folder does not exist!');
+            }
+        } else {
+            error_log('Error: Workspace ID is not valid / Might not be a number!');
+        }
+        } else {
+        error_log('Error: Workspace ID is not a number');
+        }
+        return false;
+    }
+
+    public function fetchFromXML($wsId) {
+        $return = [];
+        if(doUsefulXMLChecking($wsId)) {
+            
+        }
+    }
+    
 }
 ?>
