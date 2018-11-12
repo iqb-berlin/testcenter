0f68f458c4b0f89ebf50cca755912fc1b1047f32
andreistroescu <andrei.g.stroescu@gmail.com>
2018-11-12 16:18:15 +0100

    Added initial getAboutText php part

---
+++ php_start/getAboutText.php
@@ -0,0 +1,21 @@
+<?php
+	// preflight OPTIONS-Request bei CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  exit();
+} else {
+  
+  $myreturn = "Default text";
+
+  $filename = "./about.txt";
+
+  if (file_exists($filename)) {
+
+    $myreturn = file_get_contents($filename);
+  } 
+
+  echo(json_encode($myreturn));
+
+}
+     
+
+?>
