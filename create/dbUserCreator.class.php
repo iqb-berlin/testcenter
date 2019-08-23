aeaf382a1b8090c021ebff68317b1f2558f7a32d
paflov <paf@titelfrei.de>
2019-08-23 19:13:24 +0200

    don't exit with error if nothing happened because user was allready there

--- create/dbUserCreator.class.php
+++ create/dbUserCreator.class.php
@@ -6,22 +6,25 @@
 class dbUserCreator extends DBConnection {
 
     /**
-     * @param $username
-     * @param $userpassword
-     * @return string
-     * @throws Exception
+     *
+     * adds a new super user to db, if user table is empty (!)
+     *
+     * @param $username - name for the super user to create
+     * @param $userpassword  - password for the super user to create
+     * @return boolean - true if user was created, false if not (but o error occurred)
+     * @throws Exception - if error occurs during connection
      */
     public function addSuperuser($username, $userpassword) {
         $sql = $this->pdoDBhandle->prepare('SELECT users.name FROM users');
 
         if (!$sql->execute()) {
-            throw new Exception('could not select from users');
+            throw new Exception('Could not select from table `users` - database not initialized correctly?');
         }
 
         $data = $sql->fetchAll(PDO::FETCH_ASSOC);
 
         if (($data === false) || count($data) ) {
-            throw new Exception('user table must be empty');
+            return false;
         }
 
         $sql = $this->pdoDBhandle->prepare('INSERT INTO users (name, password, is_superadmin) VALUES (:user_name, :user_password, true)');
@@ -31,8 +34,9 @@ class dbUserCreator extends DBConnection {
         );
 
         if (!$sql->execute($params)) {
-            throw new Exception('could not select from users');
+            throw new Exception('Could not insert into table `users`');
         }
 
+        return true;
     }
 }
