9614e3e36b0839ed5a197acb1db5e2475e7b366b
paflov <paf@titelfrei.de>
2019-09-11 13:03:35 +0200

    remove error_logs

--- admin_v2/autoload.php
+++ admin_v2/autoload.php
@@ -4,7 +4,6 @@ spl_autoload_register(function($className) {
 
     $className = array_pop(explode("\\", $className));
 
-    error_log("TRY to autoload $className");
     $includeDirs = [
         ROOT_DIR . "/vo_code",
         ROOT_DIR . "/admin_v2/classes"
@@ -12,7 +11,6 @@ spl_autoload_register(function($className) {
 
     foreach ($includeDirs as $includeDir) {
         foreach (glob("$includeDir/{,*/,*/*/,*/*/*/}$className{.class.php,.php}", GLOB_BRACE) as $classFile) {
-            error_log("found class file: $classFile");
             require_once $classFile;
             return;
         }
