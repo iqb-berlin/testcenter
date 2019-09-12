4fc35801511659b8f13991b49c32cbe951e850d4
paflov <paf@titelfrei.de>
2019-09-12 14:17:04 +0200

    Restore hooks for dredd test with yml
    Revert "describe endpoint php/getFile.php"
    
    This reverts commit 169635c2

--- integration/hooks.js
+++ integration/hooks.js
@@ -1,5 +1,4 @@
 const hooks = require('hooks');
-const fs = require("fs");
 
 const stash = {};
 
@@ -13,40 +12,24 @@ hooks.beforeEachValidation(function(transaction) {
 hooks.beforeEach(function(transaction, done) {
 
     // dont' check error responses
-    // if (transaction.expected.statusCode.substr(0,1) !== "2") {
-    //     transaction.skip = true;
-    // }
+    if (transaction.expected.statusCode.substr(0,1) !== "2") {
+        transaction.skip = true;
+    }
 
     // use login credentials
     if (typeof transaction.request.headers['AuthToken'] !== "undefined") {
-        let authToken = JSON.parse(transaction.request.headers['AuthToken']);
+        let authToken =transaction.request.headers['AuthToken'];
         authToken.at = stash.authToken;
-        hooks.log("restoring auth token:" + stash.authToken );
         transaction.request.headers['AuthToken'] = JSON.stringify(authToken);
-        transaction.request.headers['restored'] = 'yessss:' + stash.authToken;
     }
     transaction.request.headers['Accept'] = "*/*";
     done();
 });
 
-hooks.after('Login > Login', function(transaction, done) {
+hooks.after('/php/login.php/login > Login > 200 > application/json', function(transaction, done) {
 
     // store login credentials
     stash.authToken = JSON.parse(transaction.real.body).admintoken;
     hooks.log("stashing auth token:" + stash.authToken );
-    done();
-});
-
-hooks.before('get file > get file', function(transaction, done) {
-
-    const atParameterRegex = /at=[\w\\.]+/gm;
-    transaction.fullPath = transaction.fullPath.replace(atParameterRegex, 'at=' + stash.authToken);
-    transaction.expected.body = fs.readFileSync('../vo_data/ws_1/Unit/SAMPLE_UNIT.XML', 'utf-8').toString();
-    done();
-});
-
-hooks.before('/php/getFile.php > get file > 200 > application/octet-stream', function(transaction, done) {
-
-    transaction.skip = true;
     done();
 });
