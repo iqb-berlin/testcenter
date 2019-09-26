b9727c49deb4103d1d545caf43136b5af41ea44c
paflov <paf@titelfrei.de>
2019-09-26 14:24:49 +0200

    return the time of call arrival ($_SERVER['REQUEST_TIME_FLOAT']) with the randomized data package

--- php_tc/doSysCheckDownloadTest.php
+++ php_tc/doSysCheckDownloadTest.php
@@ -9,17 +9,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	exit();
 }
 
-if (!isset($_GET['size']) or (!intval($_GET['size'])) or (intval($_GET['size']) > 4194304)) {
+$POST = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($POST['size']) or (!intval($POST['size'])) or (intval($POST['size']) > 4194304) or (intval($POST['size']) < 16)) {
     http_response_code(404);
-    echo "Unsupported test size ({$_GET['size']})";
+    echo "Unsupported test size ({$POST['size']})";
     exit(1);
 }
 
 header('Content-Transfer-Encoding: binary');
 header('Content-Type: text/plain');
 
+echo $_SERVER['REQUEST_TIME_FLOAT'];
+echo '/';
+
 $allowedChars = "ABCDEFGHIJKLOMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz0123456789+/";
-$length = intval($_GET['size']);
+$length = intval($POST['size']) - strlen($_SERVER['REQUEST_TIME_FLOAT']) - 1;
 while ($length-- > 1) {
     echo substr($allowedChars, rand(0, strlen($allowedChars) - 1), 1);
 }
