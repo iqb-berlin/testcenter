f246db45870f482aee3f8ceb4f1ea01e8e5d1e05
paflov <paf@titelfrei.de>
2020-01-09 16:47:14 +0100

    added more debug info

--- scripts/write_travis_config.sh
+++ scripts/write_travis_config.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo "{\"type\": \"mysql\", \"host\": \"127.0.0.1\"port\": \"3306\", \"dbname\": \"tmp\", \"user\": \"root\", \"password\": \"\" }" >> config/DBConnectionData.json
+echo config/DBConnectionData.json
