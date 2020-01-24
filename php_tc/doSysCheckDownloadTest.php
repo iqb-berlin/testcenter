2742b73254434241b3d9412d961f84c212d4aa1e
paflov <paf@titelfrei.de>
2020-01-24 15:42:12 +0100

    dont zip speed test packages

--- php_tc/doSysCheckDownloadTest.php
+++ php_tc/doSysCheckDownloadTest.php
@@ -8,24 +8,27 @@
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	exit();
 }
+$start = microtime(true);
 
-$POST = json_decode(file_get_contents('php://input'), true);
+apache_setenv('no-gzip', '1');
+//$_GET = json_decode(file_get_contents('php://input'), true);
 
-if (!isset($POST['size']) or (!intval($POST['size'])) or (intval($POST['size']) > 4194304) or (intval($POST['size']) < 16)) {
+if (!isset($_GET['size']) or (!intval($_GET['size'])) or (intval($_GET['size']) > 8388608 * 8) or (intval($_GET['size']) < 16)) {
     http_response_code(404);
-    echo "Unsupported test size ({$POST['size']})";
+    echo "Unsupported test size ({$_GET['size']})";
     exit(1);
 }
 
 header('Content-Transfer-Encoding: binary');
 header('Content-Type: text/plain');
-
 echo $_SERVER['REQUEST_TIME_FLOAT'];
 echo '/';
 
 $allowedChars = "ABCDEFGHIJKLOMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz0123456789+/";
-$length = intval($POST['size']) - strlen($_SERVER['REQUEST_TIME_FLOAT']) - 1;
+$length = intval($_GET['size']) - strlen($_SERVER['REQUEST_TIME_FLOAT']) - 1;
 while ($length-- > 1) {
     echo substr($allowedChars, rand(0, strlen($allowedChars) - 1), 1);
 }
 echo '=';
+$time_elapsed_secs = microtime(true) - $start;
+//echo $time_elapsed_secs;
