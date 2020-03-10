1083446e3b60d5104bf26ebff1892c3fbdff0492
paflov <paf@titelfrei.de>
2020-03-10 14:10:28 +0100

    * check if test is writable for user * re-put field `code'`to session

---
+++ classes/middleware/IsTestWritable.class.php
@@ -0,0 +1,31 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class IsTestWritable {
+
+    function __invoke(Request $request, Response $response, $next) {
+
+        $route = $request->getAttribute('route');
+        $params = $route->getArguments();
+
+        if (!isset($params['test_id']) or ((int) $params['test_id'] < 1)) {
+            throw new HttpBadRequestException($request, "No valid test-Id: {$params['test_id']}");
+        }
+
+        /* @var $authToken AuthToken */
+        $authToken = $request->getAttribute('AuthToken');
+
+        $dbConnection = new DBConnectionTC();
+
+        if (!$dbConnection->canWriteTestData($authToken->getToken(), $params['test_id'])) {
+            throw new HttpForbiddenException($request,"Access to test {$params['test_id']} is not provided.");
+        }
+
+        return $next($request, $response);
+    }
+}
