e46fd50ed95ad81a8f08d2f91fe486d24d167a8d
rhenck <richard.henck@tu-berlin.de>
2020-08-25 11:51:47 +0200

    Remove unused travis script

--- scripts/write_travis_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "{\"type\": \"mysql\", \"host\": \"127.0.0.1\", \"port\": \"3306\", \"dbname\": \"tmp\", \"user\": \"root\", \"password\": \"\" }" >> config/DBConnectionData.json
-more config/DBConnectionData.json
