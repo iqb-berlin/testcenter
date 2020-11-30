2b3bd0ec997949969f5905d39e9d436da176903a
paf <paf@titelfrei.de>
2020-11-30 12:57:10 +0100

    move inline functions to monitorController to satisfy DRY principle

--- routes/monitor.php
+++ routes/monitor.php
@@ -8,96 +8,11 @@ use Slim\Http\Response;
 
 $app->group('/monitor', function(App $app) {
 
-    $adminDAO = new AdminDAO();
+    $app->get('/group/{group_name}', [MonitorController::class, 'getGroup']);
 
-    $app->get('/group/{group_name}', function(Request $request, Response $response) use ($adminDAO) {
+    $app->get('/test-sessions', [MonitorController::class, 'getTestSessions']);
 
-        /* @var $authToken AuthToken */
-        $authToken = $request->getAttribute('AuthToken');
-        $groupName = $request->getAttribute('group_name');
-
-        $testtakersFolder = new TesttakersFolder($authToken->getWorkspaceId());
-
-        $group = $testtakersFolder->findGroup($groupName);
-
-        if (!$group) {
-
-            throw new HttpNotFoundException($request, "Group `$groupName` not found.");
-        }
-
-        // currently a group-monitor can always only monitor it's own group
-        if ($groupName !== $authToken->getGroup()) {
-
-            throw new HttpForbiddenException($request,"Group `$groupName` not allowed.");
-        }
-
-        return $response->withJson($group);
-    });
-
-
-    $app->get('/test-sessions', function(Request $request, Response $response) use ($adminDAO) {
-
-        /* @var $authToken AuthToken */
-        $authToken = $request->getAttribute('AuthToken');
-
-        // currently a group-monitor can always only monitor it's own group
-        $groupNames = [$authToken->getGroup()];
-
-        $sessionChangeMessages = $adminDAO->getTestSessions($authToken->getWorkspaceId(), $groupNames);
-
-        $bsUrl = BroadcastService::registerChannel('monitor', ["groups" => [$authToken->getGroup()]]);
-
-         if ($bsUrl !== null) {
-
-            foreach ($sessionChangeMessages as $sessionChangeMessage) {
-
-                BroadcastService::sessionChange($sessionChangeMessage);
-            }
-
-            $response = $response->withHeader('SubscribeURI', $bsUrl);
-        }
-
-        return $response->withJson($sessionChangeMessages->asArray());
-    });
-
-
-    $app->put('/command', function(Request $request, Response $response) use ($adminDAO) {
-
-        /* @var $authToken AuthToken */
-        $authToken = $request->getAttribute('AuthToken');
-        $personId = $authToken->getId();
-
-        $body = RequestBodyParser::getElements($request, [
-            'keyword' => null,
-            'arguments' => [],
-            'timestamp' => null,
-            'testIds' => []
-        ]);
-
-        $command = new Command(-1, $body['keyword'], (int) $body['timestamp'], ...$body['arguments']);
-
-        foreach (array_unique($body['testIds']) as $testId) {
-
-            if (!$adminDAO->getTest($testId)) {
-
-                throw new HttpNotFoundException($request, "Test `{$testId}` not found. `{$command->getKeyword()}` not committed.");
-            }
-        }
-
-        foreach ($body['testIds'] as $testId) {
-
-            $commandId = $adminDAO->storeCommand($personId, (int) $testId, $command);
-            $command->setId($commandId);
-        }
-
-        BroadcastService::send('command', json_encode([
-            'command' => $command,
-            'testIds' => $body['testIds']
-        ]));
-
-        return $response->withStatus(201);
-    });
-
+    $app->put('/command', [MonitorController::class, 'putCommand']);
 
     $app->post('/group/{group_name}/tests/unlock', [MonitorController::class, 'postUnlock']);
 })
