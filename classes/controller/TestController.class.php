1157bef8c022bb629b32a1ae7147dd9ecbe029fe
paf <paf@titelfrei.de>
2020-11-30 11:23:45 +0100

    add comment

--- classes/controller/TestController.class.php
+++ classes/controller/TestController.class.php
@@ -346,7 +346,10 @@ class TestController extends Controller {
         }
 
         $newState = self::testDAO()->updateTestState($testId, ['CONNECTION' => 'LOST']);
-        // TODO write log also (?)
+        // TODO write log also -> can not safely be written since the lack of a timestamp.
+        // We need to solve https://github.com/iqb-berlin/testcenter-backend/issues/162 first (syncing of server and
+        // client time), before we should do this.
+        // See also: https://github.com/iqb-berlin/testcenter-backend/issues/172
 
         if (!isset($testSession['group_name'])) {
             error_log(print_r($testSession, true));
