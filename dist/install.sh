82c0aa657a9e709bae2659bf6d24710fe5a262e1
rhenck <richard.henck@iqb.hu-berlin.de>
2021-04-19 17:22:33 +0200

    Update install script in dist

--- dist/install.sh
+++ dist/install.sh
@@ -33,13 +33,46 @@ else
   echo "Make found"
 fi
 
-read  -p 'Install directory: ' -e -i "`pwd`/testcenter" TARGET_DIR
+### Download package ###
+DOWNLOAD='y'
+if ls testcenter-*.tar 1> /dev/null 2>&1
+  then
+    PACKAGE_FOUND=true
+    if [ $(ls testcenter-*.tar | wc -l) -gt 1 ]
+      then
+        echo "Multiple packages found. Remove all but the one you want!"
+        exit 1
+    fi
+    read -p "Installation package found. Do you want to check for and download the latest release anyway? [Y/n]:" -e DOWNLOAD
+  else
+    PACKAGE_FOUND=false
+    read -p "No installation package found. Do you want to download the latest release? [Y/n]:" -e DOWNLOAD
+fi
+
+if [ "$PACKAGE_FOUND" = 'false' ] && [ "$DOWNLOAD" = 'n' ]
+  then
+    echo "Can not continue without install package."
+    exit 1
+fi
+
+if [[ $DOWNLOAD != "n" ]]
+  then
+    echo 'Downloading latest package...'
+    rm -f testcenter-*.tar;
+    curl -s https://api.github.com/repos/iqb-berlin/testcenter-setup/releases/latest \
+    | grep "browser_download_url.*tar" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -;
+fi
 
 ### Unpack application ###
+read  -p 'Install directory: ' -e -i "`pwd`/testcenter" TARGET_DIR
 mkdir $TARGET_DIR
 tar -xf *.tar -C $TARGET_DIR
+cd $TARGET_DIR
 
-# ### Set up config ###
+### Set up config ###
 read  -p 'Server Address (hostname or IP): ' -e -i $(hostname) HOSTNAME
 sed -i "s/localhost/$HOSTNAME/" .env
 
@@ -61,7 +94,7 @@ read  -p 'Database user password: ' -e -i $MYSQL_PASSWORD MYSQL_PASSWORD
 sed -i "s/iqb_tba_db_password/$MYSQL_PASSWORD/" .env
 
 read  -p 'Use TLS? (y/N): ' -e TLS
-if [ $TLS = 'y' ]
+if [ $TLS != 'n' ]
 then
   echo "The certificates need to be placed in config/certs and their name configured in config/cert_config.yml."
   sed -i 's/http:/https:/' .env
@@ -69,18 +102,18 @@ then
 fi
 
 ### Populate Makefile ###
-if [ $TLS = 'y' ]
+if [ $TLS != 'n' ]
 then
   rm docker-compose.prod.nontls.yml
   sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml up/' Makefile-template
-  sed -i 's/<run-datached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml up -d/' Makefile-template
+  sed -i 's/<run-detached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml up -d/' Makefile-template
   sed -i 's/<stop-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml stop/' Makefile-template
   sed -i 's/<down-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml down/' Makefile-template
   sed -i 's/<pull-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml pull/' Makefile-template
 else
   rm docker-compose.prod.tls.yml
   sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml up/' Makefile-template
-  sed -i 's/<run-datached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml up -d/' Makefile-template
+  sed -i 's/<run-detached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml up -d/' Makefile-template
   sed -i 's/<stop-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml stop/' Makefile-template
   sed -i 's/<down-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml down/' Makefile-template
   sed -i 's/<pull-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.nontls.yml pull/' Makefile-template
