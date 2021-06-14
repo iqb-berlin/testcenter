ce40e6c52841dc339f6ba8102268a33a7ecfbd22
paf <paf@titelfrei.de>
2021-06-14 12:26:57 +0200

    Add missing trailing slash in environment.ts

--- e2etest/environment.ts
+++ e2etest/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  testcenterUrl: 'http://testcenter-backend:80',
+  testcenterUrl: 'http://testcenter-backend:80/', // never, ever forget trailing slash!
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
   apiVersionExpected: '9.1.0',
   veronaApiVersionSupported: '2.1.0'
