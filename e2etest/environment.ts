5957172ab7787da969ae36b95e1cadc3e1717494
paf <paf@titelfrei.de>
2021-06-15 09:34:32 +0200

    update apiVersionExpected in environment.ts for e2e-test

--- e2etest/environment.ts
+++ e2etest/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   testcenterUrl: 'http://testcenter-backend:80/', // never, ever forget trailing slash!
   appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '9.1.0',
+  apiVersionExpected: '11.3.0',
   veronaApiVersionSupported: '2.1.0'
 };
