c8e8d737c7f77d8c4fcf237c5848e40897448a35
paf <paf@titelfrei.de>
2021-09-24 14:20:06 +0200

    Restore check if test is owned by requesting user.

---
+++ classes/middleware/IsTestWritable.class.php
@@ -0,0 +1,33 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+declare(strict_types=1);
+// TODO unit test
+
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class IsTestWritable {
+
+    function __invoke(Request $request, Response $response, $next) {
+
+        $route = $request->getAttribute('route');
+        $params = $route->getArguments();
+
+        if (!isset($params['test_id']) or ((int) $params['test_id'] < 1)) {
+            throw new HttpBadRequestException($request, "No valid test-Id: {$params['test_id']}");
+        }
+
+        /* @var $authToken AuthToken */
+        $authToken = $request->getAttribute('AuthToken');
+
+        $sessionDAO = new SessionDAO();
+
+        if (!$sessionDAO->ownsTest($authToken->getToken(), $params['test_id'])) {
+            throw new HttpForbiddenException($request,"Access to test {$params['test_id']} is not provided.");
+        }
+
+        return $next($request, $response);
+    }
+}
--- routes/test.php
+++ routes/test.php
@@ -9,27 +9,36 @@ $app->group('/test', function(App $app) {
 
     $app->put('', [TestController::class, 'put']);
 
-    $app->get('/{test_id}', [TestController::class, 'get']);
+    $app->get('/{test_id}', [TestController::class, 'get'])
+        ->add(new IsTestWritable());
 
     $app->get('/{test_id}/unit/{unit_name}[/alias/{alias}]', [TestController::class, 'getUnit']);
 
     $app->get('/{test_id}/resource/{resource_name}', [TestController::class, 'getResource']);
 
-    $app->put('/{test_id}/unit/{unit_name}/review', [TestController::class, 'putUnitReview']);
+    $app->put('/{test_id}/unit/{unit_name}/review', [TestController::class, 'putUnitReview'])
+        ->add(new IsTestWritable());
 
-    $app->put('/{test_id}/review', [TestController::class, 'putReview']);
+    $app->put('/{test_id}/review', [TestController::class, 'putReview'])
+        ->add(new IsTestWritable());
 
-    $app->put('/{test_id}/unit/{unit_name}/response', [TestController::class, 'putUnitResponse']);
+    $app->put('/{test_id}/unit/{unit_name}/response', [TestController::class, 'putUnitResponse'])
+        ->add(new IsTestWritable());
 
-    $app->patch('/{test_id}/state', [TestController::class, 'patchState']);
+    $app->patch('/{test_id}/state', [TestController::class, 'patchState'])
+        ->add(new IsTestWritable());
 
-    $app->put('/{test_id}/log', [TestController::class, 'putLog']);
+    $app->put('/{test_id}/log', [TestController::class, 'putLog'])
+        ->add(new IsTestWritable());
 
-    $app->patch('/{test_id}/unit/{unit_name}/state', [TestController::class, 'putUnitState']);
+    $app->patch('/{test_id}/unit/{unit_name}/state', [TestController::class, 'putUnitState'])
+        ->add(new IsTestWritable());
 
-    $app->put('/{test_id}/unit/{unit_name}/log', [TestController::class, 'putUnitLog']);
+    $app->put('/{test_id}/unit/{unit_name}/log', [TestController::class, 'putUnitLog'])
+        ->add(new IsTestWritable());
 
-    $app->patch('/{test_id}/lock', [TestController::class, 'patchLock']);
+    $app->patch('/{test_id}/lock', [TestController::class, 'patchLock'])
+        ->add(new IsTestWritable());
 
     $app->get('/{test_id}/commands', [TestController::class, 'getCommands']);
 
