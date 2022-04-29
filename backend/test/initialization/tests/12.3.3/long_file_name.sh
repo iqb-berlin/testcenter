5497fd60dda196cf759e7d31df7a3c63fca421a0
paflov <paf@titelfrei.de>
2022-04-29 14:26:03 +0200

    Fix error on Testtakers.XML with filename of more than 30 characters
    https://github.com/iqb-berlin/testcenter-backend/issues/239

---
+++ backend/test/initialization/tests/12.3.3/long_file_name.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source integration/test-init/functions/functions.sh
+
+echo_h1 "Long file names cause trouble. 12.3.3 should fix this finally!";
+
+# so already installed patches can be re-installed
+
+echo_h2 "Install Version 12.3.2";
+fake_version 12.3.2
+php scripts/initialize.php \
+--user_name "" \
+--workspace "workspace" \
+--host=$MYSQL_HOST \
+--port=$MYSQL_PORT \
+--dbname=$MYSQL_DATABASE \
+--user=$MYSQL_USER \
+--password=$MYSQL_PASSWORD \
+--skip_read_workspace_files=true \
+--skip_db_integrity_check=true
+expect_init_script_ok
+
+echo_h2 "12.3.2 broke on filenames 30+ characters"
+echo "<Testtakers><Metadata><Description>30+ chars in filename</Description></Metadata><Group id=\"x\" label=\"x\"><Login mode=\"run-hot-restart\" name=\"x\"><Booklet>BOOKLET.SAMPLE-1</Booklet></Login></Group></Testtakers>" \
+  > vo_data/ws_1/Testtakers/2023-04-28_V8DeuTBAPilot_LOGINS_Testtaker_FMB-A.xml
+php scripts/initialize.php
+expect_init_script_failed
+
+echo_h2 "But 12.3.3 should accept up to 120 characters";
+take_current_version
+php scripts/initialize.php
+expect_init_script_ok
+
+echo_h2 "File with name of 120+ characters gets ignored";
+echo "<Testtakers><Metadata><Description>120+ chars in filename</Description></Metadata><Group id=\"y\" label=\"y\"><Login mode=\"run-hot-restart\" name=\"y\"><Booklet>BOOKLET.SAMPLE-1</Booklet></Login></Group></Testtakers>" \
+  > vo_data/ws_1/Testtakers/123456789_1_123456789_2_123456789_3_123456789_4_123456789_5_123456789_6_123456789_7_123456789_8_123456789_9_123456789_10_123456789_A.xml
+php scripts/initialize.php
+expect_init_script_ok
---
+++ database/mysql.patches.d/12.3.3.sql
@@ -0,0 +1,2 @@
+alter table logins modify source varchar(120) not null;
+alter table files modify id varchar(120) null;
