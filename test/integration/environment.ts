1a775beb7f4711992f4e258a3d61043540a5eb77
paflov <paf@titelfrei.de>
2022-06-13 12:44:45 +0200

    Remove old protractor tests

--- test/integration/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  testcenterUrl: 'http://testcenter-backend:80/', // never, ever forget trailing slash!
-  appPublisher: 'IQB - Institut zur Qualitätsentwicklung im Bildungswesen',
-  apiVersionExpected: '11.3.0',
-  veronaApiVersionSupported: '2.1.0'
-};
