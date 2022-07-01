abcf10ec8b8daf234b240717aa5011896313dacf
paflov <paf@titelfrei.de>
2022-07-01 13:29:04 +0200

    Optimize queries even more.
    Create more useful indexes and also let the columns which are supposed to be joined have the same collation and charset.

--- mysql.patches.d/12.4.3.sql
+++ mysql.patches.d/12.4.3.sql
@@ -1,14 +1,5 @@
-create index admin_sessions_user_id_index
-    on admin_sessions (user_id);
+create index login_sessions_token_index on login_sessions (token);
+create index person_sessions_token_index on person_sessions (token);
 
-create unique index users_id_uindex
-    on users (id);
-
-create index login_sessions_name_index
-    on login_sessions (name);
-
-create index logins_name_index
-    on logins (name);
-
-create index person_sessions_login_sessions_id_index
-    on person_sessions (login_sessions_id);
+alter table logins convert to character set utf8 collate utf8_german2_ci;
+alter table logins modify name varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
