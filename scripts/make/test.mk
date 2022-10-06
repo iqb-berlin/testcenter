e77f2df7d555240e91b42d60d46311ad0a6c2680
Richard Henck <richard.henck@iqb.hu-berlin.de>
2022-10-06 15:23:08 +0200

    Separate test commands for creating coverage reports or not

--- scripts/make/test.mk
+++ scripts/make/test.mk
@@ -1,5 +1,13 @@
-# Performs unit tests of the backend (with PHPUnit) and creates code-coverage-report
 test-backend-unit:
+	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
+		--rm --no-deps --entrypoint "" \
+		testcenter-backend \
+		php -dxdebug.mode=coverage vendor/phpunit/phpunit/phpunit \
+		--bootstrap test/unit/bootstrap.php \
+		--configuration phpunit.xml \
+		test/unit/.
+
+test-backend-unit-coverage:
 	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
 		--rm --no-deps --entrypoint "" \
 		testcenter-backend \
@@ -9,12 +17,10 @@ test-backend-unit:
 		--coverage-html /docs/dist/test-coverage-backend-unit \
 		test/unit/.
 
-
 # Performs Api-Tests against in-memory DB (sqlite, for performance)
 test-backend-api:
 	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml up -d testcenter-db testcenter-backend
 	make run-task-runner task=backend:api-test
-
 
 # Performs Api-Tests against MySql (takes a long time, run manually when needed)
 test-backend-api-mysql:
@@ -23,14 +29,12 @@ test-backend-api-mysql:
 		docker-compose -f docker/docker-compose.initialization-test.yml --profile=dredd_test_against_mysql up \
 		--force-recreate --renew-anon-volumes --abort-on-container-exit
 
-
 # Performs a tests suite from the initialization tests.
 # Param test - (All files in backend/test/initialization/tests for are available tests.)
 # Example: `make test-backend-initialization test=general/db-versions`
 test-backend-initialization:
 	TEST_NAME=$(test) \
 		docker-compose -f docker/docker-compose.initialization-test.yml up --force-recreate --renew-anon-volumes --abort-on-container-exit
-
 
 # Performs some tests around the initialization script like upgrading the db-schema.
 test-backend-initialization-general:
@@ -40,22 +44,31 @@ test-backend-initialization-general:
 	make test-backend-initialization test=general/no-db-but-files
 	make test-backend-initialization test=general/install-db-patches
 
-# Insert this to copy result docs to local TODO
-#		-v $(CURDIR)/docs/dist:/docs/dist \
-# Performs unit tests with Jest for the backend. Creates a code-coverage report.
 test-broadcasting-service-unit:
 	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
 		-v $(CURDIR)/broadcasting-service/src:/app/src \
 		testcenter-broadcasting-service \
+		npx jest
+
+test-broadcasting-service-unit-coverage:
+	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
+		-v $(CURDIR)/broadcasting-service/src:/app/src \
+		-v $(CURDIR)/docs/dist:/docs/dist \
+		testcenter-broadcasting-service \
 		npx jest --coverage
 
-# Performs unit tests with Karma for the frontend. Creates a code-coverage report.
 test-frontend-unit:
 	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
 		-v $(CURDIR)/frontend/src:/app/src \
 		testcenter-frontend \
-		npx ng test --watch=false --code-coverage
+		npx ng test --watch=false
 
+test-frontend-unit-coverage:
+	docker-compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
+		-v $(CURDIR)/frontend/src:/app/src \
+		-v $(CURDIR)/docs/dist:/docs/dist \
+		testcenter-frontend \
+		npx ng test --watch=false --code-coverage
 
 # Performs some integration tests with CyPress against mocked backend with Prism
 test-frontend-integration:
