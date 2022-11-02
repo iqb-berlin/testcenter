3eda1cbc2c1589facc587ae7a000264e018f564e
rhenck <richard.henck@iqb.hu-berlin.de>
2022-11-02 22:45:37 +0100

    [frontend] Improve Settings component layout
    - Border around settings group removed. It served no purpose and was 
    misaligned.
    - Give the settings column more width.
    - Give descriptive name to css class

--- frontend/src/app/superadmin/settings/settings.component.ts
+++ frontend/src/app/superadmin/settings/settings.component.ts
@@ -3,24 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   template: `
     <div fxLayout="column" fxLayoutAlign="start stretch" class="admin-tab-content">
-      <div fxLayout="row" class="div-row">
-        <div fxFlex="30">
+      <div fxLayout="row" class="settings-group">
+        <div fxFlex="20">
           <mat-label>Text-Ersetzungen</mat-label>
         </div>
-        <div fxFlex="68">
+        <div fxFlex="78">
           <app-custom-texts></app-custom-texts>
         </div>
       </div>
-      <div fxLayout="row" class="div-row">
-        <div fxFlex="30">
+      <div fxLayout="row" class="settings-group">
+        <div fxFlex="20">
           <mat-label>Konfiguration der Anwendung</mat-label>
         </div>
-        <div fxFlex="68">
+        <div fxFlex="78">
           <app-app-config></app-app-config>
         </div>
       </div>
     </div>
   `,
-  styles: ['.div-row {border-color: gray; border-width: 0 0 1px 0; border-style: solid; margin-top: 10px}']
+  styles: ['.settings-group {margin-top: 10px;}']
 })
 export class SettingsComponent {}
