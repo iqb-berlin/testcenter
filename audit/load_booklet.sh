0a448fa54bc03934025a88460579a3506a54a786
paflov <philipp.franck@hu-berlin.de>
2022-12-22 12:30:31 +0100

    Update audit script

--- audit/load_booklet.sh
+++ audit/load_booklet.sh
@@ -1,2 +1,2 @@
 curl --location --request GET 'http://localhost/testcenter/backend/workspace/6/file/Booklet/Booklet.xml' \
---header 'AuthToken: a639b08b9cd7594.81568842'
+--header 'AuthToken: '"$AUTH_TOKEN"
