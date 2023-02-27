e15ac1a7505a1f86302087cf9ccb35c1d851af0a
rhenck <richard.henck@iqb.hu-berlin.de>
2023-02-27 11:12:47 +0100

    [Setup] Fix patch for renamed TLS config file
    - Fix indentation target yml
    - Make file executable
    - Add check for existence of renamed file; skip step automatically if 
    present

--- dist-src/patch.sh
+++ dist-src/patch.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Rename SSL-config file
-read -p "SSL config file will be renamed and additional TLS settings will be added. This may overwrite customizations. \
-Certificate paths are untouched.
+if [ ! -f config/tls-config.yml ]; then
+  read -p "SSL config file will be renamed and additional TLS settings will be added. This may overwrite customizations. \
+  Certificate paths are untouched.
 Refer to the Documentation and Changelog if you want to do this change manually.
 Continue with this step? [y/N]:" -r -n 1 -e CONTINUE
-if [[ $CONTINUE =~ [yY] ]]
-  then
-    mv config/cert_config.yml config/tls-config.yml 2>/dev/null
-    sed -i 's/tls:/tls: \
-      options: \
-        default: \
-          minVersion: VersionTLS12 \
-          cipherSuites: \
-            - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 \
-            - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384 \
-            - TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256 \
-            - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 \
-            - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 \
-            - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384/' config/tls-config.yml
+  if [[ $CONTINUE =~ [yY] ]]
+    then
+      mv config/cert_config.yml config/tls-config.yml 2>/dev/null
+      sed -i 's/tls:/tls: \
+  options: \
+    default: \
+      minVersion: VersionTLS12 \
+      cipherSuites: \
+        - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 \
+        - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384 \
+        - TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256 \
+        - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 \
+        - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 \
+        - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384/' config/tls-config.yml
+  fi
 fi
 
 
