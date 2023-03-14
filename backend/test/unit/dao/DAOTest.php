8ad481739ebb219447eec4ba112478c79d2a9a4b
paflov <philipp.franck@hu-berlin.de>
2023-03-14 10:03:22 +0100

    BE: Apply new coding style

--- backend/test/unit/dao/DAOTest.php
+++ backend/test/unit/dao/DAOTest.php
@@ -3,113 +3,99 @@
 
 use PHPUnit\Framework\TestCase;
 
-
 /**
  * @runTestsInSeparateProcesses
  * @preserveGlobalState disabled
  */
 class DAOTest extends TestCase {
+  private DAO $dbc;
 
-    private DAO $dbc;
+  function setUp(): void {
+    require_once "src/exception/HttpError.class.php";
+    require_once "src/data-collection/DataCollection.class.php";
+    require_once "src/helper/DB.class.php";
+    require_once "src/helper/JSON.class.php";
+    require_once "src/data-collection/DBConfig.class.php";
+    require_once "src/dao/DAO.class.php";
 
-    function setUp(): void {
+    DB::connect(new DBConfig(["type" => "temp"]));
+    $this->dbc = new DAO();
+    $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/database.sql');
+    $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/unit/testdata.sql');
+  }
 
-        require_once "src/exception/HttpError.class.php";
-        require_once "src/data-collection/DataCollection.class.php";
-        require_once "src/helper/DB.class.php";
-        require_once "src/helper/JSON.class.php";
-        require_once "src/data-collection/DBConfig.class.php";
-        require_once "src/dao/DAO.class.php";
+  function tearDown(): void {
+    unset($this->dbc);
+  }
 
-        DB::connect(new DBConfig(["type" => "temp"]));
-        $this->dbc = new DAO();
-        $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/database.sql');
-        $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/unit/testdata.sql');
-    }
+  function test_getDBSchemaVersion() {
+    $result = $this->dbc->getDBSchemaVersion();
+    $this->assertEquals('0.0.0-no-entry', $result, 'No entry in meta table');
 
+    $this->dbc->_("insert into meta (metaKey, value) values ('dbSchemaVersion', '10.0.0')");
+    $result = $this->dbc->getDBSchemaVersion();
+    $this->assertEquals('10.0.0', $result, 'Version present');
 
-    function tearDown(): void {
+    $this->dbc->_("drop table meta");
+    $result = $this->dbc->getDBSchemaVersion();
+    $this->assertEquals('0.0.0-no-table', $result, 'No meta table present');
+  }
 
-        unset($this->dbc);
-    }
+  public function test_getMeta() {
+    $result = $this->dbc->getMeta(['cat1']);
+    $expectation = [
+      'cat1' => [
+        'keyA' => 'valueA',
+        'keyB' => 'valueB'
+      ]
+    ];
+    $this->assertEquals($expectation, $result);
 
+    $result = $this->dbc->getMeta(['cat1', 'cat2']);
+    $expectation = [
+      'cat1' => [
+        'keyA' => 'valueA',
+        'keyB' => 'valueB'
+      ],
+      'cat2' => [
+        'keyA' => 'valueA',
+        'keyB' => 'valueB'
+      ]
+    ];
+    $this->assertEquals($expectation, $result);
+  }
 
-    function test_getDBSchemaVersion() {
+  public function test_setMeta() {
+    $this->dbc->setMeta('new', 'aKey', 'aValue');
+    $result = $this->dbc->getMeta(['new']);
+    $expectation = [
+      'new' => [
+        'aKey' => 'aValue',
+      ]
+    ];
+    $this->assertEquals($expectation, $result);
 
-        $result = $this->dbc->getDBSchemaVersion();
-        $this->assertEquals('0.0.0-no-entry', $result, 'No entry in meta table');
+    $this->dbc->setMeta('new', 'aKey', 'newValue');
+    $result = $this->dbc->getMeta(['new']);
+    $expectation = [
+      'new' => [
+        'aKey' => 'newValue',
+      ]
+    ];
+    $this->assertEquals($expectation, $result);
+  }
 
-        $this->dbc->_("INSERT INTO meta (metaKey, value) VALUES ('dbSchemaVersion', '10.0.0')");
-        $result = $this->dbc->getDBSchemaVersion();
-        $this->assertEquals('10.0.0', $result, 'Version present');
+  public function test_getTestFullState() {
+    $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => true, "running" => true]);
+    $this->assertSame(["A" => "B", "status" => 'locked'], $result);
 
-        $this->dbc->_("Drop Table meta");
-        $result = $this->dbc->getDBSchemaVersion();
-        $this->assertEquals('0.0.0-no-table', $result, 'No meta table present');
-    }
+    $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => false, "running" => false]);
+    $this->assertSame(["A" => "B", "status" => 'pending'], $result);
 
+    $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => false, "running" => true]);
+    $this->assertSame(["A" => "B", "status" => 'running'], $result);
 
-    public function test_getMeta() {
-
-        $result = $this->dbc->getMeta(['cat1']);
-        $expectation = [
-            'cat1' => [
-                'keyA' => 'valueA',
-                'keyB' => 'valueB'
-            ]
-        ];
-        $this->assertEquals($expectation, $result);
-
-
-        $result = $this->dbc->getMeta(['cat1', 'cat2']);
-        $expectation = [
-            'cat1' => [
-                'keyA' => 'valueA',
-                'keyB' => 'valueB'
-            ],
-            'cat2' => [
-                'keyA' => 'valueA',
-                'keyB' => 'valueB'
-            ]
-        ];
-        $this->assertEquals($expectation, $result);
-    }
-
-
-    public function test_setMeta() {
-
-        $this->dbc->setMeta('new', 'aKey', 'aValue');
-        $result = $this->dbc->getMeta(['new']);
-        $expectation = [
-            'new' => [
-                'aKey' => 'aValue',
-            ]
-        ];
-        $this->assertEquals($expectation, $result);
-
-        $this->dbc->setMeta('new', 'aKey', 'newValue');
-        $result = $this->dbc->getMeta(['new']);
-        $expectation = [
-            'new' => [
-                'aKey' => 'newValue',
-            ]
-        ];
-        $this->assertEquals($expectation, $result);
-    }
-
-
-    public function test_getTestFullState() {
-
-        $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => true, "running" => true]);
-        $this->assertSame(["A" => "B", "status" => 'locked'], $result);
-
-        $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => false, "running" => false]);
-        $this->assertSame(["A" => "B", "status" => 'pending'], $result);
-
-        $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => false, "running" => true]);
-        $this->assertSame(["A" => "B", "status" => 'running'], $result);
-
-        $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => true, "running" => false]);
-        $this->assertSame(["A" => "B", "status" => 'locked'], $result);
-    }
+    $result = $this->dbc->getTestFullState(['testState' => '{"A":"B"}', "locked" => true, "running" => false]);
+    $this->assertSame(["A" => "B", "status" => 'locked'], $result);
+  }
 }
--- backend/test/unit/dao/TestDAOTest.php
+++ backend/test/unit/dao/TestDAOTest.php
@@ -1,6 +1,7 @@
 <?php /** @noinspection PhpUnhandledExceptionInspection */
 
 use PHPUnit\Framework\TestCase;
+
 require_once "src/exception/HttpError.class.php";
 require_once "src/data-collection/DataCollection.class.php";
 require_once "src/helper/DB.class.php";
@@ -11,205 +12,187 @@ require_once "src/data-collection/Command.class.php";
 require_once "src/dao/DAO.class.php";
 require_once "src/dao/TestDAO.class.php";
 
-
 class TestDAOTest extends TestCase {
+  private TestDAO $dbc;
 
-    private TestDAO $dbc;
+  function setUp(): void {
+    DB::connect(new DBConfig(["type" => "temp"]));
+    $this->dbc = new TestDAO();
+    $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/database.sql');
+    $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/unit/testdata.sql');
+  }
 
-    function setUp(): void {
-
-        DB::connect(new DBConfig(["type" => "temp"]));
-        $this->dbc = new TestDAO();
-        $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/database.sql');
-        $this->dbc->runFile(REAL_ROOT_DIR . '/backend/test/unit/testdata.sql');
-    }
-
-
-    function tearDown(): void {
-
+  function tearDown(): void {
 //        print_r($this->dbc->getDBContentDump());
-        unset($this->dbc);
-    }
+    unset($this->dbc);
+  }
 
+  function test_getTestState() {
+    $expected = ['CURRENT_UNIT_ID' => 'UNIT_1'];
+    $result = $this->dbc->getTestState(1);
 
-    function test_getTestState() {
+    $this->assertEquals($expected, $result);
 
-        $expected = ['CURRENT_UNIT_ID' => 'UNIT_1'];
-        $result = $this->dbc->getTestState(1);
+    $expected = [];
+    $result = $this->dbc->getTestState(3);
 
-        $this->assertEquals($expected, $result);
+    $this->assertEquals($expected, $result);
+  }
 
-        $expected = [];
-        $result = $this->dbc->getTestState(3);
+  function test_getUnitState() {
+    $expected = ["SOME_STATE" => "WHATEVER"];
+    $result = $this->dbc->getUnitState(1, 'UNIT_1');
+    $this->assertEquals($expected, $result);
 
-        $this->assertEquals($expected, $result);
-    }
+    $expected = [];
+    $result = $this->dbc->getUnitState(999, 'UNIT_1');
+    $this->assertEquals($expected, $result);
 
+    $expected = [];
+    $result = $this->dbc->getUnitState(1, 'not_existing_unit');
+    $this->assertEquals($expected, $result);
+  }
 
-    function test_getUnitState() {
+  function test_updateTestState() {
+    $testState = [
+      "some_entry" => 'some_content',
+      "with_encoded_json_content" => '{"a":"b"}',
+    ];
 
-        $expected = ["SOME_STATE" => "WHATEVER"];
-        $result = $this->dbc->getUnitState(1, 'UNIT_1');
-        $this->assertEquals($expected, $result);
+    $expected = [
+      'CURRENT_UNIT_ID' => 'UNIT_1',
+      'some_entry' => 'some_content',
+      'with_encoded_json_content' => '{"a":"b"}'
+    ];
 
-        $expected = [];
-        $result = $this->dbc->getUnitState(999, 'UNIT_1');
-        $this->assertEquals($expected, $result);
+    $result = $this->dbc->updateTestState(1, $testState);
+    $this->assertEquals($expected, $result);
 
-        $expected = [];
-        $result = $this->dbc->getUnitState(1, 'not_existing_unit');
-        $this->assertEquals($expected, $result);
-    }
+    $resultFromGet = $this->dbc->getTestState(1);
+    $this->assertEquals($result, $resultFromGet);
 
+    $updateState = [
+      "some_entry" => 'new_content',
+      "new_entry" => 'anything',
+    ];
 
-    function test_updateTestState() {
+    $expectedAfterUpdate = [
+      'CURRENT_UNIT_ID' => 'UNIT_1',
+      'some_entry' => 'new_content',
+      'with_encoded_json_content' => '{"a":"b"}',
+      "new_entry" => 'anything'
+    ];
 
-        $testState = [
-            "some_entry" => 'some_content',
-            "with_encoded_json_content" => '{"a":"b"}',
-        ];
+    $resultAfterUpdate = $this->dbc->updateTestState(1, $updateState);
+    $this->assertEquals($expectedAfterUpdate, $resultAfterUpdate);
 
-        $expected = [
-            'CURRENT_UNIT_ID' => 'UNIT_1',
-            'some_entry' => 'some_content',
-            'with_encoded_json_content' => '{"a":"b"}'
-        ];
+    $resultFromGetAfterUpdate = $this->dbc->getTestState(1);
+    $this->assertEquals($resultAfterUpdate, $resultFromGetAfterUpdate);
+  }
 
-        $result = $this->dbc->updateTestState(1, $testState);
-        $this->assertEquals($expected, $result);
+  function test_updateUnitState() {
+    $testState = [
+      "some_entry" => 'some_content',
+      "with_encoded_json_content" => '{"a":"b"}',
+    ];
 
-        $resultFromGet = $this->dbc->getTestState(1);
-        $this->assertEquals($result, $resultFromGet);
+    $expected = [
+      'SOME_STATE' => 'WHATEVER',
+      'some_entry' => 'some_content',
+      'with_encoded_json_content' => '{"a":"b"}'
+    ];
 
-        $updateState = [
-            "some_entry" => 'new_content',
-            "new_entry" => 'anything',
-        ];
+    $result = $this->dbc->updateUnitState(1, 'UNIT_1', $testState);
+    $this->assertEquals($expected, $result);
 
-        $expectedAfterUpdate = [
-            'CURRENT_UNIT_ID' => 'UNIT_1',
-            'some_entry' => 'new_content',
-            'with_encoded_json_content' => '{"a":"b"}',
-            "new_entry" => 'anything'
-        ];
+    $resultFromGet = $this->dbc->getUnitState(1, 'UNIT_1');
+    $this->assertEquals($result, $resultFromGet);
 
-        $resultAfterUpdate = $this->dbc->updateTestState(1, $updateState);
-        $this->assertEquals($expectedAfterUpdate, $resultAfterUpdate);
+    $updateState = [
+      "some_entry" => 'new_content',
+      "new_entry" => 'anything',
+    ];
 
-        $resultFromGetAfterUpdate = $this->dbc->getTestState(1);
-        $this->assertEquals($resultAfterUpdate, $resultFromGetAfterUpdate);
-    }
+    $expectedAfterUpdate = [
+      'SOME_STATE' => 'WHATEVER',
+      'some_entry' => 'new_content',
+      'with_encoded_json_content' => '{"a":"b"}',
+      "new_entry" => 'anything',
+    ];
 
+    $resultAfterUpdate = $this->dbc->updateUnitState(1, 'UNIT_1', $updateState);
+    $this->assertEquals($expectedAfterUpdate, $resultAfterUpdate);
 
-    function test_updateUnitState() {
+    $resultFromGetAfterUpdate = $this->dbc->getUnitState(1, 'UNIT_1');
+    $this->assertEquals($resultAfterUpdate, $resultFromGetAfterUpdate);
+  }
 
-        $testState = [
-            "some_entry" => 'some_content',
-            "with_encoded_json_content" => '{"a":"b"}',
-        ];
+  function test_getCommands() {
+    $expected = [
+      new Command(1, 'COMMAND_C', 1597903000),
+      new Command(3, 'COMMAND_D', 1597904000, "param1", "param2"),
+    ];
+    $result = $this->dbc->getCommands(1, 4);
 
-        $expected = [
-            'SOME_STATE' => 'WHATEVER',
-            'some_entry' => 'some_content',
-            'with_encoded_json_content' => '{"a":"b"}'
-        ];
+    $this->assertEquals($expected, $result);
 
-        $result = $this->dbc->updateUnitState(1, 'UNIT_1', $testState);
-        $this->assertEquals($expected, $result);
+    $expected = [
+      new Command(2, 'COMMAND_A', 1597900000, "param1"),
+      new Command(4, 'COMMAND_B', 1597901000),
+      new Command(1, 'COMMAND_C', 1597903000),
+      new Command(3, 'COMMAND_D', 1597904000, "param1", "param2"),
+    ];
+    $result = $this->dbc->getCommands(1);
 
-        $resultFromGet = $this->dbc->getUnitState(1, 'UNIT_1');
-        $this->assertEquals($result, $resultFromGet);
+    $this->assertEquals($expected, $result);
 
-        $updateState = [
-            "some_entry" => 'new_content',
-            "new_entry" => 'anything',
-        ];
+    $expected = [
+      new Command(1, 'COMMAND_X', 1597902000)
+    ];
+    $result = $this->dbc->getCommands(2);
+    $this->assertEquals($expected, $result);
 
-        $expectedAfterUpdate = [
-            'SOME_STATE' => 'WHATEVER',
-            'some_entry' => 'new_content',
-            'with_encoded_json_content' => '{"a":"b"}',
-            "new_entry" => 'anything',
-        ];
+    $expected = [];
+    $result = $this->dbc->getCommands(3);
+    $this->assertEquals($expected, $result);
 
-        $resultAfterUpdate = $this->dbc->updateUnitState(1, 'UNIT_1', $updateState);
-        $this->assertEquals($expectedAfterUpdate, $resultAfterUpdate);
+    $expected = [];
+    $result = $this->dbc->getCommands(1, 3);
+    $this->assertEquals($expected, $result);
+  }
 
-        $resultFromGetAfterUpdate = $this->dbc->getUnitState(1, 'UNIT_1');
-        $this->assertEquals($resultAfterUpdate, $resultFromGetAfterUpdate);
-    }
+  function test_getDataParts() {
+    $expected = [
+      "dataParts" => [
+        "all" => '{"name":"Elias Example","age":35}',
+        "other" => '{"other":"stuff"}'
+      ],
+      "dataType" => 'the-response-type'
+    ];
+    $result = $this->dbc->getDataParts(1, 'UNIT.SAMPLE');
+    $this->assertEquals($expected, $result);
+  }
 
-
-    function test_getCommands() {
-
-        $expected = [
-            new Command(1, 'COMMAND_C', 1597903000),
-            new Command(3, 'COMMAND_D', 1597904000, "param1", "param2"),
-        ];
-        $result = $this->dbc->getCommands(1, 4);
-
-        $this->assertEquals($expected, $result);
-
-        $expected = [
-            new Command(2, 'COMMAND_A', 1597900000, "param1"),
-            new Command(4, 'COMMAND_B', 1597901000),
-            new Command(1, 'COMMAND_C', 1597903000),
-            new Command(3, 'COMMAND_D', 1597904000, "param1", "param2"),
-        ];
-        $result = $this->dbc->getCommands(1);
-
-        $this->assertEquals($expected, $result);
-
-        $expected = [
-            new Command(1, 'COMMAND_X', 1597902000)
-        ];
-        $result = $this->dbc->getCommands(2);
-        $this->assertEquals($expected, $result);
-
-        $expected = [];
-        $result = $this->dbc->getCommands(3);
-        $this->assertEquals($expected, $result);
-
-        $expected = [];
-        $result = $this->dbc->getCommands(1, 3);
-        $this->assertEquals($expected, $result);
-    }
-
-
-    function test_getDataParts() {
-
-        $expected = [
-            "dataParts" => [
-                "all" => '{"name":"Elias Example","age":35}',
-                "other" => '{"other":"stuff"}'
-            ],
-            "dataType" => 'the-response-type'
-        ];
-        $result = $this->dbc->getDataParts(1, 'UNIT.SAMPLE');
-        $this->assertEquals($expected, $result);
-    }
-
-
-    function test_updateDataParts() {
-        $this->dbc->updateDataParts(
-            1,
-            'UNIT.SAMPLE',
-            [
-                "other" =>  '{"other": "overwritten"}',
-                "added" => '{"stuff": "added"}'
-            ],
-            'the-response-type',
-            123456789123
-        );
-        $expected = [
-            "dataParts" => [
-                "all" => '{"name":"Elias Example","age":35}',
-                "other" =>  '{"other": "overwritten"}',
-                "added" => '{"stuff": "added"}'
-            ],
-            "dataType" => 'the-response-type'
-        ];
-        $result = $this->dbc->getDataParts(1, 'UNIT.SAMPLE');
-        $this->assertEquals($expected, $result);
-    }
+  function test_updateDataParts() {
+    $this->dbc->updateDataParts(
+      1,
+      'UNIT.SAMPLE',
+      [
+        "other" => '{"other": "overwritten"}',
+        "added" => '{"stuff": "added"}'
+      ],
+      'the-response-type',
+      123456789123
+    );
+    $expected = [
+      "dataParts" => [
+        "all" => '{"name":"Elias Example","age":35}',
+        "other" => '{"other": "overwritten"}',
+        "added" => '{"stuff": "added"}'
+      ],
+      "dataType" => 'the-response-type'
+    ];
+    $result = $this->dbc->getDataParts(1, 'UNIT.SAMPLE');
+    $this->assertEquals($expected, $result);
+  }
 }
