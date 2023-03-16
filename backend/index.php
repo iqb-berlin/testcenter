c87e24f676e01878eb9c32a5b196f50c1343393e
paflov <philipp.franck@hu-berlin.de>
2023-03-16 13:57:07 +0100

    Apply new coding style

--- backend/index.php
+++ backend/index.php
@@ -7,60 +7,53 @@ use Slim\Factory\AppFactory;
 use Slim\Http\ServerRequest as Request;
 use Slim\Http\Response;
 
-
 try {
+  date_default_timezone_set('Europe/Berlin'); // just to be safe. TimeStamp-class should be used everywhere anyway
 
-    date_default_timezone_set('Europe/Berlin'); // just to be safe. TimeStamp-class should be used everywhere anyway
+  define('ROOT_DIR', realpath(dirname(__FILE__) . '/../'));
 
-    define('ROOT_DIR', realpath(dirname(__FILE__) . '/../'));
+  require_once "vendor/autoload.php";
+  require_once "autoload.php";
 
-    require_once "vendor/autoload.php";
-    require_once "autoload.php";
+  if (isset($_SERVER['HTTP_TESTMODE'])) {
+    TestEnvironment::setup($_SERVER['HTTP_TESTMODE']);
+  } else { // productive
+    /* @var $config SystemConfig */
+    $config = SystemConfig::fromFile(ROOT_DIR . '/backend/config/system.json');
+    define('DATA_DIR', ROOT_DIR . '/data');
+    TimeStamp::setup();
+    BroadcastService::setup($config->broadcastServiceUriPush, $config->broadcastServiceUriSubscribe);
+    XMLSchema::setup($config->allowExternalXMLSchema);
+    DB::connect();
+  }
 
-    if (isset($_SERVER['HTTP_TESTMODE'])) {
+  $container = new Container();
+  AppFactory::setContainer($container);
+  $app = AppFactory::create();
 
-        TestEnvironment::setup($_SERVER['HTTP_TESTMODE']);
+  $app->addRoutingMiddleware();
+  $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+  $errorMiddleware->setDefaultErrorHandler(new ErrorHandler());
 
-    } else { // productive
+  $projectPath = Server::getProjectPath();
+  if ($projectPath) {
+    $app->setBasePath($projectPath);
+  }
 
-        /* @var $config SystemConfig */
-        $config = SystemConfig::fromFile(ROOT_DIR . '/backend/config/system.json');
-        define('DATA_DIR', ROOT_DIR . '/data');
-        TimeStamp::setup();
-        BroadcastService::setup($config->broadcastServiceUriPush, $config->broadcastServiceUriSubscribe);
-        XMLSchema::setup($config->allowExternalXMLSchema);
+  $app->options('/{routes:.+}', function(Request $request, Response $response) {
+    return $response;
+  });
 
-        DB::connect();
-    }
+  include_once 'routes.php';
 
-    $container = new Container();
-    AppFactory::setContainer($container);
-    $app = AppFactory::create();
-
-    $app->addRoutingMiddleware();
-    $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-    $errorMiddleware->setDefaultErrorHandler(new ErrorHandler());
-
-    $projectPath = Server::getProjectPath();
-    if ($projectPath) {
-        $app->setBasePath($projectPath);
-    }
-
-    $app->options('/{routes:.+}', function(Request $request, Response $response) {
-        return $response;
-    });
-
-    include_once 'routes.php';
-
-    $app->run();
+  $app->run();
 
 } catch (Throwable $e) {
-
-    // this can only happen if slim itself or slim error handler fails or some class fails in constructor
-    http_response_code(500);
-    $id = uniqid('fatal-', true);
-    header('Error-ID:' . $id);
-    error_log("$id (500) at {$e->getFile()}:{$e->getLine()}");
-    error_log($e->getMessage());
-    echo "Fatal error!" . "$id (500) at {$e->getFile()}:{$e->getLine()} : {$e->getMessage()}";
+  // this can only happen if slim itself or slim error handler fails or some class fails in constructor
+  http_response_code(500);
+  $id = uniqid('fatal-', true);
+  header('Error-ID:' . $id);
+  error_log("$id (500) at {$e->getFile()}:{$e->getLine()}");
+  error_log($e->getMessage());
+  echo "Fatal error!" . "$id (500) at {$e->getFile()}:{$e->getLine()} : {$e->getMessage()}";
 }
