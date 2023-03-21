5c0b46408fa93cf55b52b56201c0e41b4e2604bf
paflov <philipp.franck@hu-berlin.de>
2023-03-21 12:49:37 +0100

    e2e: Repair system-tests by using the correct TestMode

--- e2e/cypress/e2e/utils.ts
+++ e2e/cypress/e2e/utils.ts
@@ -18,7 +18,7 @@ export const resetBackendData = (): void => {
   // this resets the DB because in system-test TESTMODE_REAL_DATA is true
   cy.request({
     url: `${Cypress.env('TC_API_URL')}/version`,
-    headers: { TestMode: 'quick-prepare' }
+    headers: { TestMode: 'prepare' }
   })
     .its('status').should('eq', 200);
   // sometimes DB isn't ready even after the endpoint returned 200
