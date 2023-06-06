b510cefe4b18a042395f64b5e88f7cbec25b8a04
paflov <philipp.franck@hu-berlin.de>
2023-06-06 09:40:31 +0200

    be: remove legacy workaround from times without docker

--- backend/src/controller/SpeedtestController.class.php
+++ backend/src/controller/SpeedtestController.class.php
@@ -11,9 +11,7 @@ class SpeedtestController extends Controller {
   public static function getRandomPackage(Request $request, Response $response): Response {
     $size = (int) $request->getAttribute('size');
 
-    if (function_exists('apache_setenv')) { // for OCBA server
-      apache_setenv('no-gzip', '1');
-    }
+    apache_setenv('no-gzip', '1');
 
     if (($size > 8388608 * 8) or ($size < 16)) {
       throw new HttpError("Unsupported test size ($size)", 406);
@@ -31,7 +29,6 @@ class SpeedtestController extends Controller {
     return $response
       ->withHeader('Content-Transfer-Encoding', 'binary')
       ->withHeader('Content-Type', 'text/plain');
-
   }
 
   public static function postRandomPackage(/** @noinspection PhpUnusedParameterInspection */ Request $request, Response $response): Response {
