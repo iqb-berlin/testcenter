b91178754ed6c0ed6a3baa144b0db57580d22f7d
paflov <philipp.franck@hu-berlin.de>
2023-09-12 14:42:56 +0200

    be: Add script that can create massive amounts of sessions
    Also a test, that tests if initialize takes a okayish amount of time to read those.

---
+++ backend/test/initialization/tests/14.10.0/start_with_massive_data.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source backend/test/initialization/functions/functions.sh
+
+echo_h1 "The StartUp Process should not take so long even if database is full";
+
+echo_h2 "Install";
+php backend/initialize.php \
+--user_name=super \
+--user_password=user123 \
+--workspace=new_workspace \
+--host=$MYSQL_HOST \
+--port=$MYSQL_PORT \
+--dbname=$MYSQL_DATABASE \
+--user=$MYSQL_USER \
+--password=$MYSQL_PASSWORD
+expect_init_script_ok
+
+echo_h2 "Create massive data"
+php /var/www/backend/test/massive-test-data.php \
+--workspaces=10 \
+--ttfiles_per_workspace=3 \
+--codes_per_login=30 \
+--start_test_probability=0 \
+--lock_test_probability=0
+
+
+echo_h2 "Run Init-Script Again and see how long it takes";
+timeout --kill-after=1 10m \
+php backend/initialize.php \
+--user_name=super \
+--user_password=user123 \
+--workspace=new_workspace \
+--host=$MYSQL_HOST \
+--port=$MYSQL_PORT \
+--dbname=$MYSQL_DATABASE \
+--user=$MYSQL_USER \
+--password=$MYSQL_PASSWORD
+expect_init_script_ok
