4c72d860b491ea5ece86f2319e084b9c69764082
paflov <philipp.franck@hu-berlin.de>
2023-09-22 11:20:10 +0200

    Add script which is used fore creating a new version (committing, tagging, pushing images and so on)

---
+++ scripts/create-new-version.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+if [ "$1" == '' ]; then
+  echo "usage: bash scripts/create-new-version.sh [major|minor|patch][-{label}]"
+  echo "example: bash scripts/create_new_version.sh minor"
+  exit 1;
+fi
+
+if [ "$(git symbolic-ref --short HEAD)" != "master" ]; then
+  echo "Not on master!";
+  exit 1;
+fi
+
+make docs-user
+make new-version version=$1
+
+git add dist-src/.env
+git add docs/CHANGELOG.md
+git add docs/pages/*
+git add package.json
+git add package-lock.json
+git add sampledata/*
+git add scripts/database/patches.d/*
+
+if [ ! -e scripts/database/next.sql ]; then
+  if [ "$(git status | grep -c scripts/database/next.sql)" -gt 0 ]; then
+    git rm scripts/database/next.sql
+  fi
+fi
+
+git status
+
+VERSION=$(npm pkg get version | xargs echo)
+
+read -n1 -p "Commit Version $VERSION? (Y/n) " confirm
+if ! echo $confirm | grep '^[Yy]\?$'; then
+  exit 0
+fi
+
+git commit -m "Update to version $VERSION"
+git tag $VERSION
+git push origin master
+git push origin $VERSION
+
+
+read -n1 -p "Push Images Version $VERSION manually? (y/N) " confirm
+if echo $confirm | grep '^[Yy]\?$'; then
+  exit 0
+fi
+
+docker build --target prod -t "iqbberlin/testcenter-backend:$VERSION" -f docker/backend.Dockerfile .
+docker build --target prod -t "iqbberlin/testcenter-frontend:$VERSION" -f docker/frontend.Dockerfile .
+docker build --target prod -t "iqbberlin/testcenter-broadcasting-service:$VERSION" -f docker/broadcasting-service.Dockerfile .
+
+docker login -u "iqbberlin4cicd"
+
+docker push iqbberlin/testcenter-backend:$VERSION
+docker push iqbberlin/testcenter-frontend:$VERSION
+docker push iqbberlin/testcenter-broadcasting-service:$VERSION
+
+echo "Now go to to https://github.com/iqb-berlin/testcenter/releases and create the new release".
