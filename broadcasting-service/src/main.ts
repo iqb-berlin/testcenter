54ca7e59861d9b691b70071a0c8e0cd373b43c65
paflov <philipp.franck@hu-berlin.de>
2023-09-28 14:38:47 +0200

    bs: Don't expose framework.

--- broadcasting-service/src/main.ts
+++ broadcasting-service/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const isDevelopment = !!process.env.DEV_MODE;
-  const app = await NestFactory.create(AppModule, { logger: isDevelopment ? console : ['warn', 'error'] });
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    {
+      logger: isDevelopment ? console : ['warn', 'error']
+    }
+  );
+  app.disable('x-powered-by', 'X-Powered-By');
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(3000);
   // eslint-disable-next-line no-console
