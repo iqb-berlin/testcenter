2b333dc5c7008346a5b9007944f6b0eb0d5bfa58
paflov <philipp.franck@hu-berlin.de>
2023-10-17 13:57:09 +0200

    be: Fix update sql scheme script
    which is used when building a new version.

--- backend/test/update-sql-scheme.php
+++ backend/test/update-sql-scheme.php
@@ -1,4 +1,6 @@
 <?php
 define('ROOT_DIR', realpath(__DIR__ . '/../..'));
+require_once(ROOT_DIR . '/backend/vendor/autoload.php');
 require_once 'unit/TestDB.class.php';
+SystemConfig::readFromEnvironment();
 TestDB::setUp();
