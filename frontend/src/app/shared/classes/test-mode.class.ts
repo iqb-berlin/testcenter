45125841d09325f077c058b8f4c0eac6da53e286
paflov <philipp.franck@hu-berlin.de>
2024-02-13 12:06:48 +0100

    fe: Fix loss of testmodeId and Label

--- frontend/src/app/shared/classes/test-mode.class.ts
+++ frontend/src/app/shared/classes/test-mode.class.ts
@@ -2,8 +2,8 @@ import { TestModeData } from 'testcenter-common/classes/test-mode-data.class';
 import { AppError } from '../../app.interfaces';
 
 export class TestMode extends TestModeData {
-  modeId: keyof typeof TestModeData.modes = 'RUN-DEMO';
-  modeLabel: string = 'Demo';
+  readonly modeId: keyof typeof TestModeData.modes = 'RUN-DEMO';
+  readonly modeLabel: string = 'Demo';
 
   constructor(loginMode: string = 'RUN-DEMO') {
     super();
@@ -17,14 +17,14 @@ export class TestMode extends TestModeData {
       });
     }
 
-    this.modeId = mode;
-    this.modeLabel = TestModeData.labels[mode];
-
     (Object.keys(this) as Array<keyof typeof TestModeData.modes[typeof mode]>)
       .forEach((key: keyof typeof TestModeData.modes[typeof mode]) => {
         if (key in this) {
           this[key] = TestModeData.modes[mode][key];
         }
       });
+
+    this.modeId = mode;
+    this.modeLabel = TestModeData.labels[mode];
   }
 }
