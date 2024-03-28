934603308caf6b654af6e3b6c1d533a75568409c
paflov <philipp.franck@hu-berlin.de>
2024-03-28 14:13:50 +0100

    env: fix patch

--- dist-src/patches/15.1.0.sh
+++ dist-src/patches/15.1.0.sh
@@ -11,7 +11,7 @@ wget -nv -O docker-compose.yml https://raw.githubusercontent.com/${REPO_URL}/${V
 wget -nv -O docker-compose.prod.yml https://raw.githubusercontent.com/${REPO_URL}/${VERSION}/dist-src/docker-compose.prod.yml
 
 # Download additional compose file
-wget -nv -O docker-compose.prod.yml https://raw.githubusercontent.com/${REPO_URL}/${VERSION}/dist-src/docker-compose.prod.tls.yml
+wget -nv -O docker-compose.prod.tls.yml https://raw.githubusercontent.com/${REPO_URL}/${VERSION}/dist-src/docker-compose.prod.tls.yml
 
 # Change Makefile for non-tls setup
 wget -nv -O Makefile https://raw.githubusercontent.com/${REPO_URL}/${VERSION}/dist-src/Makefile
