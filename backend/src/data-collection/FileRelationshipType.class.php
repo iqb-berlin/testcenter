726ed4bbf83eed6ee89317873d2250ba1e85f33a
paflov <philipp.franck@hu-berlin.de>
2024-05-16 14:05:57 +0200

    be: Add usesScheme to FileRelationshipType

--- backend/src/data-collection/FileRelationshipType.class.php
+++ backend/src/data-collection/FileRelationshipType.class.php
@@ -1,12 +1,12 @@
 <?php
 declare(strict_types=1);
 
-
 enum FileRelationshipType {
-    case hasBooklet;
-    case containsUnit;
-    case usesPlayer;
-    case usesPlayerResource;
-    case isDefinedBy;
-    case unknown;
+  case hasBooklet;
+  case containsUnit;
+  case usesPlayer;
+  case usesPlayerResource;
+  case isDefinedBy;
+  case usesScheme;
+  case unknown;
 }
