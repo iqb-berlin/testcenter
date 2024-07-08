488b9d47bdaeef8820b80f1ae353fe1509960931
paflov <philipp.franck@hu-berlin.de>
2024-07-08 14:55:20 +0200

    Trigger UnitInaccessiblePipe at the right moment

--- frontend/src/app/test-controller/pipes/unit-inaccessible.pipe.ts
+++ frontend/src/app/test-controller/pipes/unit-inaccessible.pipe.ts
@@ -1,13 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Unit } from '../interfaces/test-controller.interfaces';
+import { TestletLockType, Unit } from '../interfaces/test-controller.interfaces';
 import { TestControllerService } from '../services/test-controller.service';
 
 @Pipe({
   name: 'unit_inaccessible'
 })
 export class UnitInaccessiblePipe implements PipeTransform {
+  // lockedDirectly and locked are part of unit,
+  // but we take it as parameters to detect changes corretcly
+  // since subobjects are not tracked by the pipe
+  // https://docs.angular.lat/guide/pipes#detecting-pure-changes-to-primitives-and-object-references
   // eslint-disable-next-line class-methods-use-this
-  transform(unit: Unit): boolean {
+  transform(unit: Unit, lockedDirectly: boolean, locked?: TestletLockType): boolean {
     return TestControllerService.unitIsInaccessible(unit);
   }
 }
