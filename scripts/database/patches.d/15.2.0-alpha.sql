e1284a4083f241cf8bafa1c11c8319c0cec0c410
paflov <philipp.franck@hu-berlin.de>
2024-08-20 10:18:13 +0200

    Sort db patches

--- scripts/database/patches.d/15.2.0-alpha.sql
+++
@@ -1,2 +0,0 @@
-alter table file_relations
-  modify relationship_type enum ('hasBooklet', 'containsUnit', 'usesPlayer', 'usesPlayerResource', 'isDefinedBy', 'usesScheme', 'unknown') not null;
--- scripts/database/patches.d/next.sql
+++ scripts/database/patches.d/next.sql
@@ -1,2 +1,7 @@
 alter table workspaces
   add column workspace_hash varchar(255) not null default '';
+
+
+-- 15.3
+alter table file_relations
+  modify relationship_type enum ('hasBooklet', 'containsUnit', 'usesPlayer', 'usesPlayerResource', 'isDefinedBy', 'usesScheme', 'unknown') not null;
