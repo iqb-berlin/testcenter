bd782ab55cf50775d4ffcad87472c53eed79988a
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2024-09-11 11:38:14 +0200

    send sysCheckReport immediately, if logged in
    - if logged in, the 'name' of the login is inserted into the sysCheckReport as 'title'/schulID
    - the keyPhrase or the 'sys-check-report' password is ommitted in this case
    - output savereport popup only for not logged in sys-check

--- frontend/src/app/sys-check/report/report.component.ts
+++ frontend/src/app/sys-check/report/report.component.ts
@@ -6,6 +6,7 @@ import { SaveReportComponent } from './save-report/save-report.component';
 import { ReportEntry } from '../sys-check.interfaces';
 import { ConfirmDialogComponent } from '../../shared/components/confirm-dialog/confirm-dialog.component';
 import { ConfirmDialogData } from '../../shared/interfaces/confirm-dialog.interfaces';
+import { MainDataService } from '../../shared/services/maindata/maindata.service';
 
 @Component({
   templateUrl: './report.component.html',
@@ -18,44 +19,61 @@ export class ReportComponent implements OnInit {
   constructor(
     private backendService: BackendService,
     public dataService: SysCheckDataService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private mds: MainDataService
   ) {
   }
 
   saveReport(): void {
-    const dialogRef = this.dialog.open(SaveReportComponent, {
-      width: '500px',
-      height: '600px'
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (typeof result !== 'undefined') {
-        if (result !== false) {
-          const reportKey = result.get('key').value as string;
-          const reportTitle = result.get('title').value as string;
-          this.backendService.saveReport(
-            this.dataService.checkConfig.workspaceId,
-            this.dataService.checkConfig.name,
-            {
-              keyPhrase: reportKey,
-              title: reportTitle,
-              environment: this.dataService.environmentReport,
-              network: this.dataService.networkReport,
-              questionnaire: this.dataService.questionnaireReport,
-              unit: []
-            }
-          ).subscribe(() => {
-            this.dialog.open(ConfirmDialogComponent, {
-              width: '400px',
-              data: <ConfirmDialogData>{
-                title: 'Bericht gespeichert',
-                content: 'Der Bericht wurde erfolgreich gespeichert.',
-                confirmbuttonlabel: 'Verstanden'
-              }
-            });
-          });
-        }
+    const confirmDialog = () => this.dialog.open(ConfirmDialogComponent, {
+      width: '400px',
+      data: <ConfirmDialogData>{
+        title: 'Bericht gespeichert',
+        content: 'Der Bericht wurde erfolgreich gespeichert.',
+        confirmbuttonlabel: 'Verstanden'
       }
     });
+    if (!this.mds.sysCheckAvailableForAll) {
+      this.backendService.saveReport(
+        this.dataService.checkConfig.workspaceId,
+        this.dataService.checkConfig.name,
+        {
+          environment: this.dataService.environmentReport,
+          network: this.dataService.networkReport,
+          questionnaire: this.dataService.questionnaireReport,
+          unit: []
+        }
+      ).subscribe(() => {
+        confirmDialog();
+      });
+    } else {
+      const dialogRef = this.dialog.open(SaveReportComponent, {
+        width: '500px',
+        height: '600px'
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        if (typeof result !== 'undefined') {
+          if (result !== false) {
+            const reportKey = result.get('key').value as string;
+            const reportTitle = result.get('title').value as string;
+            this.backendService.saveReport(
+              this.dataService.checkConfig.workspaceId,
+              this.dataService.checkConfig.name,
+              {
+                keyPhrase: reportKey,
+                title: reportTitle,
+                environment: this.dataService.environmentReport,
+                network: this.dataService.networkReport,
+                questionnaire: this.dataService.questionnaireReport,
+                unit: []
+              }
+            ).subscribe(() => {
+              confirmDialog();
+            });
+          }
+        }
+      });
+    }
   }
 
   ngOnInit(): void {
--- frontend/src/app/sys-check/sys-check.interfaces.ts
+++ frontend/src/app/sys-check/sys-check.interfaces.ts
@@ -88,8 +88,8 @@ export interface DetectedNetworkInformation {
 }
 
 export interface SysCheckReport {
-  keyPhrase: string;
-  title: string;
+  keyPhrase?: string;
+  title?: string;
   environment: ReportEntry[];
   network: ReportEntry[];
   questionnaire: ReportEntry[];
