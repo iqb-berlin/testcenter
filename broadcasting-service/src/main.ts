f5135f027fa6f2b2947d4fbee79056c1075c8b52
paflov <philipp.franck@hu-berlin.de>
2024-09-25 10:48:34 +0200

    Reduce useless logs from Broadcasting-Service
    closes #175

--- broadcasting-service/src/main.ts
+++ broadcasting-service/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { BsLogger } from './logger';
 
 async function bootstrap() {
-  const isDevelopment = !!process.env.DEV_MODE;
+  const isDevelopment = process.env.DEV_MODE === 'true';
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
     {
-      logger: isDevelopment ? console : ['warn', 'error']
+      logger: isDevelopment ? new BsLogger() : ['warn', 'error']
     }
   );
   app.disable('x-powered-by', 'X-Powered-By');
