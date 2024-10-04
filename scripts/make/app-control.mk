a6216c9e3b71cdb7e93f7a820de0bf15098e7719
svwolter <sven.wolter@iqb.hu-berlin.de>
2024-10-04 16:28:37 +0200

    [Setup] Revise install script

--- scripts/make/app-control.mk
+++ scripts/make/app-control.mk
@@ -1,12 +1,12 @@
-#TC_BASE_DIR := $(shell git rev-parse --show-toplevel)
-TC_BASE_DIR := .
+TC_BASE_DIR := $(shell git rev-parse --show-toplevel)
 
 ## prevents collisions of make target names with possible file names
 .PHONY: init build run down start stop logs build-prod-local up-prod-local down-prod-local logs-prod-local
 
 # Initialized the Application. Run this right after checking out the Repo.
 init:
-	cp $(TC_BASE_DIR)/docker/default.env $(TC_BASE_DIR)/docker/.env
+	cp $(TC_BASE_DIR)/docker/.env.dev-template $(TC_BASE_DIR)/docker/.env.dev
+	cp $(TC_BASE_DIR)/dist-src/.env.prod-template $(TC_BASE_DIR)/dist-src/.env.prod
 	cp $(TC_BASE_DIR)/frontend/src/environments/environment.dev.ts $(TC_BASE_DIR)/frontend/src/environments/environment.ts
 	chmod 0755 $(TC_BASE_DIR)/scripts/database/000-create-test-db.sh
 	mkdir -m 777 -p $(TC_BASE_DIR)/docs/dist
@@ -16,37 +16,62 @@ init:
 # Param: (optional) service - Only build a specified service, e.g. `service=testcenter-backend`
 build:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --progress plain --file docker/docker-compose.yml --file docker/docker-compose.dev.yml build $(service)
+	docker compose\
+			--progress plain\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		build $(service)
 
 # Ramp the application up (i.e. creates and starts all application containers).
 # Hint: Stop local webserver before, to free port 80
 # Param: (optional) service - Only ramp up a specified service, e.g. `service=testcenter-backend`
 run:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml up --detach $(service)
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		up --detach $(service)
 
 # Stop and remove all application containers.
 down:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml down --remove-orphans $(service)
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		down --remove-orphans $(service)
 
 # Start the application with already existing containers.
 # Param: (optional) service - Only start a specified service, e.g. `service=testcenter-backend`
 start:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml start $(service)
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		start $(service)
 
 # Stop the application but don't remove the service containers.
 # Param: (optional) service - Only stop a specified service, e.g. `service=testcenter-backend`
 stop:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml stop $(service)
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		stop $(service)
 
 # Log the application.
 # Param: (optional) service - Only log a specified service, e.g. `service=testcenter-backend`
 logs:
 	cd $(TC_BASE_DIR) &&\
-	docker compose --file docker/docker-compose.yml --file docker/docker-compose.dev.yml logs --follow $(service)
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		logs --follow $(service)
 
 # Build all images of the project or a specified one as prod-images.
 # Param: (optional) service - Only build a specified service, e.g. `service=testcenter-backend`
@@ -54,6 +79,7 @@ build-prod-local:
 	cd $(TC_BASE_DIR) &&\
 	docker compose\
 			--progress plain\
+			--env-file docker/.env.dev\
 			--file docker/docker-compose.yml\
 			--file docker/docker-compose.dev.yml\
 			--file docker/docker-compose.local-prod.yml\
@@ -66,6 +92,7 @@ up-prod-local:
 	cd $(TC_BASE_DIR) &&\
 	docker compose\
 			--progress plain\
+			--env-file docker/.env.dev\
 			--file docker/docker-compose.yml\
 			--file docker/docker-compose.dev.yml\
 			--file docker/docker-compose.local-prod.yml\
@@ -76,6 +103,7 @@ up-prod-local:
 down-prod-local:
 	cd $(TC_BASE_DIR) &&\
 	docker compose\
+			--env-file docker/.env.dev\
 			--file docker/docker-compose.yml\
 			--file docker/docker-compose.dev.yml\
 			--file docker/docker-compose.local-prod.yml\
@@ -86,6 +114,7 @@ down-prod-local:
 logs-prod-local:
 	cd $(TC_BASE_DIR) &&\
 	docker compose\
+			--env-file docker/.env.dev\
 			--file docker/docker-compose.yml\
 			--file docker/docker-compose.dev.yml\
 			--file docker/docker-compose.local-prod.yml\
--- scripts/make/misc.mk
+++ scripts/make/misc.mk
@@ -1,21 +1,56 @@
 # Performs a single task on the whole project using the task-runner
 # Param: task - For available tasks see scripts in see /package.json # TODO make clear wich ones are for task runner and which ones are for local usage
 run-task-runner:
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
-		--rm --no-deps \
-		testcenter-task-runner npm run $(task)
+	docker compose\
+ 			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		run --rm --no-deps testcenter-task-runner\
+			npm run $(task)
 
 # Copies files from the containers to local. This is useful for development in an IDE environment.
 # Container must be run at least once!
 sync-package-files:
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-frontend:/usr/src/testcenter/frontend/package.json frontend/package.json
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-frontend:/usr/src/testcenter/frontend/package-lock.json frontend/package-lock.json
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-frontend:/usr/src/testcenter/frontend/node_modules frontend/node_modules
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/package.json broadcasting-service/package.json
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/package-lock.json broadcasting-service/package-lock.json
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/node_modules broadcasting-service/node_modules
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-backend:/var/www/testcenter/backend/vendor backend
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml cp testcenter-backend:/var/www/testcenter/backend/composer.lock backend/composer.lock
+	docker compose\
+ 			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-frontend:/usr/src/testcenter/frontend/package.json frontend/package.json
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-frontend:/usr/src/testcenter/frontend/package-lock.json frontend/package-lock.json
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-frontend:/usr/src/testcenter/frontend/node_modules frontend/node_modules
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/package.json broadcasting-service/package.json
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/package-lock.json broadcasting-service/package-lock.json
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-broadcasting-service:/usr/src/testcenter/broadcasting-service/node_modules broadcasting-service/node_modules
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-backend:/var/www/testcenter/backend/vendor backend
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		cp testcenter-backend:/var/www/testcenter/backend/composer.lock backend/composer.lock
 
 update-docs:
 	make docs-frontend-compodoc
@@ -40,8 +75,12 @@ docs-user:
 # Creates some interfaces for booklets and test-modes out of the definitions.
 create-interfaces:
 	docker container rm -f testcenter-task-runner 2> /dev/null || true
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run --name=testcenter-task-runner \
-		testcenter-task-runner npx --yes update-browserslist-db@latest && npm run create-interfaces
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+		run --name=testcenter-task-runner testcenter-task-runner\
+			npx --yes update-browserslist-db@latest && npm run create-interfaces
 	# can not  use compose here https://github.com/docker/compose/issues/8467
 	docker cp testcenter-task-runner:/usr/src/testcenter/task-runner/package.json ./package.json
 	docker cp testcenter-task-runner:/usr/src/testcenter/task-runner/package-lock.json ./package-lock.json
@@ -54,7 +93,11 @@ composer-install:
 			--volume $(CURDIR)/backend/test:/usr/src/testcenter/backend/test\
 			--volume $(CURDIR)/backend/vendor:/usr/src/testcenter/backend/vendor\
 			--volume $(HOME)/.composer:/tmp/cache\
-		composer:lts install --no-interaction --ignore-platform-reqs --no-ansi --working-dir=/usr/src/testcenter/backend
+		composer:lts install\
+				--no-interaction\
+				--ignore-platform-reqs\
+				--no-ansi\
+				--working-dir=/usr/src/testcenter/backend
 	make build service=testcenter-backend
 
 composer-update:
@@ -65,7 +108,11 @@ composer-update:
 			--volume $(CURDIR)/backend/test:/usr/src/testcenter/backend/test\
 			--volume $(CURDIR)/backend/vendor:/usr/src/testcenter/backend/vendor\
 			--volume $(HOME)/.composer:/tmp/cache\
-		composer:lts update --no-interaction --ignore-platform-reqs --no-ansi --working-dir=/usr/src/testcenter/backend
+		composer:lts update\
+				--no-interaction\
+				--ignore-platform-reqs\
+				--no-ansi\
+				--working-dir=/usr/src/testcenter/backend
 	make build service=testcenter-backend
 
 # use this whenever you created or renamed a class in backend to refresh the autoloader.
@@ -81,10 +128,12 @@ composer-refresh-autoload:
 	make build service=testcenter-backend
 
 new-version:
-	docker compose -f docker/docker-compose.yml -f docker/docker-compose.dev.yml run \
- 		--rm --entrypoint="" \
- 		testcenter-backend \
-    php /var/www/testcenter/backend/test/update-sql-scheme.php
+	docker compose\
+			--env-file docker/.env.dev\
+			--file docker/docker-compose.yml\
+			--file docker/docker-compose.dev.yml\
+ 		run --rm --entrypoint="" testcenter-backend\
+    	php /var/www/testcenter/backend/test/update-sql-scheme.php
 	make run-task-runner task="new-version $(version)"
 
 # Re-runs the initialization script of the backend to apply new database patches and re-read the data-dir.
@@ -94,5 +143,7 @@ re-init-backend:
 # Use this param to only show issues which can be solved by updating
 #--ignore-unfixed
 image-scan:
-	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:latest image \
-    --security-checks vuln $(image):$(tag)
+	docker run\
+			--rm\
+			--volume /var/run/docker.sock:/var/run/docker.sock\
+		aquasec/trivy:latest image --security-checks vuln $(image):$(tag)
