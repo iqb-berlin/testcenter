27065fc24cb1fc4c6907f5ab239f0709e4e4158e
paflov <philipp.franck@hu-berlin.de>
2024-10-08 14:26:17 +0200

    Enhance prompts in new version script

--- scripts/create-new-version.sh
+++ scripts/create-new-version.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [ "$1" == '' ]; then
-  echo "usage: bash scripts/create-new-version.sh [major|minor|patch][-{label}]"
+  echo "usage: bash scripts/create-new-version.sh [major|minor|patch|{current-version}][-{label}]"
   echo "example: bash scripts/create_new_version.sh minor"
+  echo "example: bash scripts/create_new_version.sh 15.3.0-alpha3"
   exit 1;
 fi
 
@@ -52,19 +53,15 @@ fi
 git commit -m "Update to version $VERSION"
 git push origin "$(git branch --show-current)"
 
+read -n1 -p "[GitHub]: Create pull request from `feature/$VERSION`" confirm
+# TODO : automate
 
-read -n1 -p "☕ NOW WAIT until CI is ready. Proceed? (y/N)" confirm
-if ! echo "$confirm" | grep '^[Yy]\?$'; then
-  exit 0
-fi
-read -n1 -p "create pull request, merge. Proceed? (y/N)" confirm #TODO
-if ! echo "$confirm" | grep '^[Yy]\?$'; then
-  exit 0
-fi
-read -n1 -p "☕ WAIT AGAIN until CI is ready. Proceed? (y/N)" confirm
-if ! echo "$confirm" | grep '^[Yy]\?$'; then
-  exit 0
-fi
+read -n1 -p "[GitLab]: ☕ NOW WAIT until CI is ready." confirm
+
+read -n1 -p "[GitHub]: Merge PR." confirm
+# TODO : automate
+
+read -n1 -p "[GitLab]: ☕ WAIT AGAIN until CI is ready." confirm
 
 git checkout master
 git pull
@@ -75,4 +72,6 @@ git push origin $VERSION
 git branch -D release/$VERSION
 git push origin --delete release/$VERSION
 
-echo "Now go to to https://github.com/iqb-berlin/testcenter/releases and create the new release Dont forget to attach install.sh." #TODO
+echo "[GitHub]: Go to to https://github.com/iqb-berlin/testcenter/releases and create the new release"
+echo "Dont forget to attach install.sh."
+# TODO : automate
