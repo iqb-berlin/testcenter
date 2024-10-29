7af26437b1f1c73940fbf8e36dd421d470cf5e69
svwolter <sven.wolter@iqb.hu-berlin.de>
2024-10-29 19:07:13 +0100

    [Setup] Revise version 15.3.0 migration script due to old update script

--- scripts/migration/15.3.0.sh
+++ scripts/migration/15.3.0.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
 declare APP_NAME="testcenter"
+declare REPO_URL="https://raw.githubusercontent.com/iqb-berlin/${APP_NAME}"
 
 printf "Applying patch: 15.3.0 ...\n"
 
-# Add specialized config subdirectories
+# Create updated app dir structure
+mkdir -p ./backup/release
+mkdir -p ./backup/temp
+mkdir -p ./config/traefik
+mkdir -p ./scripts/make
+mkdir -p ./scripts/migration
+mkdir -p ./secrets/traefik/certs/letsencrypt
+
+# Download updated compose files
+wget -nv -O docker-compose.yml ${REPO_URL}/${VERSION}/docker/docker-compose.yml
+wget -nv -O docker-compose.prod.yml ${REPO_URL}/${VERSION}/dist-src/docker-compose.prod.yml
+wget -nv -O docker-compose.prod.tls.yml ${REPO_URL}/${VERSION}/dist-src/docker-compose.prod.tls.yml
+
+# Download new Makefile
+wget -nv -O scripts/make/"${APP_NAME}".mk ${REPO_URL}/${VERSION}/scripts/make/prod.mk
+sed -i.bak "s#scripts/update.sh#scripts/update_${APP_NAME}.sh#" scripts/make/"${APP_NAME}".mk
+printf "include scripts/make/%s.mk\n" "${APP_NAME}" >Makefile
+
+# Download new update file
+wget -nv -O scripts/update_"${APP_NAME}".sh ${REPO_URL}/${VERSION}/scripts/update.sh
+if [ -f update.sh ]; then
+  rm update.sh
+fi
+
+# Download new traefik config file
+wget -nv -O config/traefik/tls-config.yml ${REPO_URL}/${VERSION}/config/traefik/tls-config.yml
 if [ -f config/tls-config.yml ]; then
-  mkdir -p config/traefik/
-  mv config/tls-config.yml config/traefik/tls-config.yml
+  mv config/tls-config.yml config/traefik/tls-config.yml_bkp
 fi
 
 # Move certificates to secrets directory
 if [ -d config/certs/ ]; then
-  mkdir -p secrets/traefik
   mv config/certs/ secrets/traefik/
 fi
 
-# Add scripts directory
-mkdir -p scripts/make
-mkdir -p scripts/migration
-
-mv Makefile scripts/make/"${APP_NAME}".mk
-printf "include scripts/make/%s.mk\n" "${APP_NAME}" >Makefile
-
-if [ -f update.sh ]; then
-  mv update.sh scripts/update_"${APP_NAME}".sh
-  sed -i.bak "s#scripts/update.sh#scripts/update_${APP_NAME}.sh#" scripts/make/$APP_NAME.mk &&
-    rm scripts/make/$APP_NAME.mk.bak
-fi
-
 # Rename docker environment file
-mv .env .env."${APP_NAME}"
-
-# Add backup directory
-mkdir -p backup/release
-mkdir -p backup/temp
+mv .env .env.prod
 
 printf "Patch 15.3.0 applied.\n"
