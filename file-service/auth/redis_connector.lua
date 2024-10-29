2aa1f4e3a4bd14ed727b80375681ae42224dea3b
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2024-10-29 15:52:46 +0100

    simplify dns resolver settings for nginx-lua script
    resolves z #636

--- file-service/auth/redis_connector.lua
+++ file-service/auth/redis_connector.lua
@@ -1,35 +1,10 @@
 local redis_connector = {}
 
 function redis_connector.connect()
-  -- resolve host name
-  local ip = ngx.shared.dns_cache:get("testcenter-cache-service")
-
-  if (not ip) then
-    local dns = require "resty.dns.resolver"
-
-    local resolver = dns:new({
-      nameservers = {"127.0.0.11"}, -- default docker dns
-      retrans = 5,
-      timeout = 500,
-    })
-
-    local ips, err = resolver:query("testcenter-cache-service")
-    ip = ips[1]["address"]
-    if (#ips == 0) or (not ip) then
-      ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
-      ngx.say("Failed to resolve Redis hostname")
-      ngx.log(ngx.ERR, "Failed to resolve Redis hostname: ", err)
-      return ngx.exit(ngx.status)
-    end
-
-    ngx.log(ngx.INFO, "Looked up cache-service IP: " .. ip)
-    ngx.shared.dns_cache:set("testcenter-cache-service", ip)
-  end
-
   -- connect to redis
   local redis = require "resty.redis"
   local red = redis:new()
-  local ok, err = red:connect(ip, 6379, {
+  local ok, err = red:connect(os.getenv("CACHE_SERVICE_ADDRESS_FOR_LUA"), 6379, {
     pool_size = 20
   })
   if not ok then
