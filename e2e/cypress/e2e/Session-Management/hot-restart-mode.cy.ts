33690821b7271d99cb7f9925344af89cd2b8bc1e
tobhu1981 <tobias.huste@hu-berlin.de>
2025-02-20 09:58:00 +0100

    optimize testcases hot-modes session management
    * reduced to the essentials testcases
    * remove interceptions, now its a bit faster than before

--- e2e/cypress/e2e/Session-Management/hot-restart-mode.cy.ts
+++ e2e/cypress/e2e/Session-Management/hot-restart-mode.cy.ts
@@ -1,11 +1,12 @@
 import {
-  getResultFileRows, convertResultsSeperatedArrays,
+  convertResultsSeperatedArrays,
   deleteDownloadsFolder,
-  getFromIframe, loginSuperAdmin,
-  loginTestTaker, logoutTestTaker, openSampleWorkspace,
+  getFromIframe,
+  loginSuperAdmin,
+  loginTestTaker,
+  openSampleWorkspace,
   resetBackendData,
   visitLoginPage,
-  forwardTo,
   logoutAdmin
 
 } from '../utils';
@@ -24,56 +25,40 @@ describe('Check hot-restart-mode functions', { testIsolation: false }, () => {
     visitLoginPage();
   });
 
-  it('should be possible to start a hot-restart-mode study as login: hres1', () => {
+  it('should be possible to start a hot-restart session: hres1', () => {
     loginTestTaker('hres1', '203', 'test-hot');
     cy.get('[data-cy="unit-title"]')
       .contains('Aufgabe1');
-    cy.intercept(`${Cypress.env('urls').backend}/test/3/unit/UNIT.SAMPLE-101/response`).as('response-1');
     getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
       .click()
-      // todo: wenn nur response abgefangen wird, ist die Zeit zu kurz, die Checkbox ist nicht aktiv obwohl angeklickt.
-      // Es müsste noch auf state abgefangen werden, dann ist die Zeit etwas länger und die Checkbox ist aktiv.
-      // Der Status kommt dummerweise allerdings nicht immer.
-      .wait(1000)
       .should('be.checked');
-    cy.wait('@response-1');
-    forwardTo('Aufgabe2');
-    cy.intercept(`${Cypress.env('urls').backend}/test/3/unit/UNIT.SAMPLE-102/response`).as('response-2');
-    getFromIframe('[data-cy="TestController-radio2-Aufg2"]')
-      .click()
-      // todo: wenn nur response abgefangen wird, ist die Zeit zu kurz, die Checkbox ist nicht aktiv obwohl angeklickt.
-      // Es müsste noch auf state abgefangen werden, dann ist die Zeit etwas länger und die Checkbox ist aktiv.
-      // Der Status kommt dummerweise allerdings nicht immer.
-      .wait(1000)
-      .should('be.checked');
-    cy.wait('@response-2');
-    logoutTestTaker('hot');
+    cy.get('[data-cy="logo"]')
+      .click();
+    cy.log('end test');
+    cy.get('[data-cy="endTest-1"]')
+      .click();
+    cy.get('[data-cy="card-login-name"]')
+      .contains('h5ki-bd-');
+    cy.get('[data-cy="logout"]')
+      .click();
   });
 
   it('should not possible to continue the session from login: hres1, it must be start a new session', () => {
     loginTestTaker('hres1', '203', 'test-hot');
     cy.get('[data-cy="unit-title"]')
       .contains('Aufgabe1');
-    cy.intercept(`${Cypress.env('urls').backend}/test/4/unit/UNIT.SAMPLE-101/response`).as('response-1');
     getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
       .click()
-      // todo: wenn nur response abgefangen wird, ist die Zeit zu kurz, die Checkbox ist nicht aktiv obwohl angeklickt.
-      // Es müsste noch auf state abgefangen werden, dann ist die Zeit etwas länger und die Checkbox ist aktiv.
-      // Der Status kommt dummerweise allerdings nicht immer.
-      .wait(1000)
       .should('be.checked');
-    cy.wait('@response-1');
-    forwardTo('Aufgabe2');
-    cy.intercept(`${Cypress.env('urls').backend}/test/4/unit/UNIT.SAMPLE-102/response`).as('response-2');
-    getFromIframe('[data-cy="TestController-radio1-Aufg2"]')
-      .click()
-      // todo: wenn nur response abgefangen wird, ist die Zeit zu kurz, die Checkbox ist nicht aktiv obwohl angeklickt.
-      // Es müsste noch auf state abgefangen werden, dann ist die Zeit etwas länger und die Checkbox ist aktiv.
-      // Der Status kommt dummerweise allerdings nicht immer.
-      .wait(1000)
-      .should('be.checked');
-    cy.wait('@response-2');
-    logoutTestTaker('hot');
+    cy.get('[data-cy="logo"]')
+      .click();
+    cy.log('end test');
+    cy.get('[data-cy="endTest-1"]')
+      .click();
+    cy.get('[data-cy="card-login-name"]')
+      .contains('va4dg-jc');
+    cy.get('[data-cy="logout"]')
+      .click();
   });
 
   it('should be a generated file (responses, logs) in the workspace with groupname: SM_HotModes', () => {
@@ -93,33 +78,8 @@ describe('Check hot-restart-mode functions', { testIsolation: false }, () => {
     convertResultsSeperatedArrays('responses')
       .then(LoginID => {
         idHres1 = LoginID[1][2];
-        idHres2 = LoginID[3][2];
+        idHres2 = LoginID[2][2];
         expect(idHres1).to.not.equal(idHres2);
-      });
-  });
-
-  it('should be saved recent replies from first login: hres1 in downloaded response file', () => {
-    getResultFileRows('responses')
-      .then(responses => {
-        expect(responses[1]).to.be.match(/\bhres1\b/);
-        expect(responses[1]).to.be.match(/\bUNIT.SAMPLE-101\b/);
-
-        expect(responses[1]).to.be.match(/\bid"":""radio1"",""status"":""VALUE_CHANGED"",""value"":""true\b/);
-        expect(responses[2]).to.be.match(/\bhres1\b/);
-        expect(responses[2]).to.be.match(/\bUNIT.SAMPLE-102\b/);
-        expect(responses[2]).to.be.match((/\bid"":""radio2"",""status"":""VALUE_CHANGED"",""value"":""true\b/));
-      });
-  });
-
-  it('should be saved recent replies from second login: hres1 in downloaded response file', () => {
-    getResultFileRows('responses')
-      .then(responses => {
-        expect(responses[3]).to.be.match(/\bhres1\b/);
-        expect(responses[3]).to.be.match(/\bUNIT.SAMPLE-101\b/);
-        expect(responses[3]).to.be.match((/\bid"":""radio1"",""status"":""VALUE_CHANGED"",""value"":""true\b/));
-        expect(responses[4]).to.be.match(/\bhres1\b/);
-        expect(responses[4]).to.be.match(/\bUNIT.SAMPLE-102\b/);
-        expect(responses[4]).to.be.match((/\bid"":""radio1"",""status"":""VALUE_CHANGED"",""value"":""true\b/));
       });
   });
 });
