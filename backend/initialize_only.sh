bec8174e5f96fd428c29d790eee06d3e826c0784
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2025-02-21 13:56:43 +0100

    add init process as job
    - job of executing initialize.php now starts everytime a `helm install` or `helm upgrade` is run
    - the backend service pods are assumed to only execute `apache2-foreground`
    
    The successful working of this setup was tested with a nfs setup functioning as the provisioner of a persistent volume that enables the Mode ReadWriteMany. Inspiration for the test setup by https://github.com/phcollignon/nfs-server-minikube?tab=readme-ov-file

---
+++ backend/initialize_only.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional parameters based on environment variables
+params=""
+
+if [ "$OVERWRITE_INSTALLATION" = "yes" ]; then
+  params+=" --overwrite_existing_installation"
+fi
+
+if [ "$SKIP_DB_INTEGRITY" = "yes" ]; then
+  params+=" --skip_db_integrity_check"
+fi
+
+if [ "$SKIP_READ_FILES" = "yes" ]; then
+  params+=" --skip_read_workspace_files"
+fi
+
+if [ "$NO_SAMPLE_DATA" = "yes" ]; then
+  params+=" --dont_create_sample_data"
+fi
+
+# init data with conditions based on environment variables
+php /var/www/testcenter/backend/initialize.php $params
+
+# file-rights
+chown -R www-data:www-data /var/www/testcenter/data
+chown -R www-data:www-data /var/www/testcenter/backend/config
