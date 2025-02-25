5776fb86b41fe3e67081e1d588e9a8f62ece7396
svwolter <sven.wolter@iqb.hu-berlin.de>
2025-02-25 14:02:20 +0100

    [backend] Remove 'dead' Cache Service property

--- backend/src/helper/SystemConfig.class.php
+++ backend/src/helper/SystemConfig.class.php
@@ -13,7 +13,6 @@ class SystemConfig {
   public static string $broadcastingService_internal = "";
   public static string $cacheService_host = "";
   public static string $cacheService_includeFiles = "";
-  public static int $cacheService_ram = 0;
   public static string $password_salt = "t";
   public static bool $system_tlsEnabled = true;
   public static string $system_hostname;
@@ -98,7 +97,6 @@ class SystemConfig {
     }
 
     $config['cacheService']['includeFiles'] = self::boolEnv('CACHE_SERVICE_INCLUDE_FILES');
-    $config['cacheService']['ram'] = (int) self::stringEnv('CACHE_SERVICE_RAM');
 
     $overrideConfig = getenv('OVERRIDE_CONFIG');
     if ($overrideConfig) {
