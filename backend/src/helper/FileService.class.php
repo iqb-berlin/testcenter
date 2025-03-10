34d49514546029210411bdd19700c3142586217f
svwolter <sven.wolter@iqb.hu-berlin.de>
2025-03-10 11:13:51 +0100

    [file-service] Run 'file service' container rootless

--- backend/src/helper/FileService.class.php
+++ backend/src/helper/FileService.class.php
@@ -6,11 +6,11 @@ declare(strict_types=1);
 
 class FileService {
   static function getStatus(): string {
-    if (!SystemConfig::$fileService_external or !SystemConfig::$fileService_internal) {
+    if (!SystemConfig::$fileService_host) {
       return 'off';
     }
 
-    $uri = 'http://' . SystemConfig::$fileService_internal . '/health';
+    $uri = 'http://' . SystemConfig::$fileService_host . 'health';
 
     $curl = curl_init();
     curl_setopt_array($curl, [
@@ -46,10 +46,9 @@ class FileService {
   }
 
   public static function getUri(): string {
-    if (!SystemConfig::$fileService_external) {
-      return Server::getUrl() . '/';
+    if (!SystemConfig::$fileService_host) {
+      return '';
     }
-    $proto = (SystemConfig::$system_tlsEnabled ? 'https://' : 'http://');
-    return $proto . SystemConfig::$fileService_external;
+    return 'http://' . SystemConfig::$fileService_host;
   }
 }
