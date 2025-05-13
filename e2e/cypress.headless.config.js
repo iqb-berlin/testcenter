428f2136ce31f0c4626a90e59e1151d376da1dfb
svwolter <sven.wolter@iqb.hu-berlin.de>
2025-05-13 12:32:43 +0200

    Run backend with port '8080' for ordinary user processes

--- e2e/cypress.headless.config.js
+++ e2e/cypress.headless.config.js
@@ -6,7 +6,7 @@ const deleteFolder = require('./src/plugins/delete-folder');
 const waitForServer = require('./src/plugins/wait-for-server');
 
 const urls = {
-  backend: 'http://testcenter-backend',
+  backend: 'http://testcenter-backend:8080',
   fileService: 'http://testcenter-file-service',
   frontend: 'http://testcenter-frontend:4200',
   broadcastingService: 'http://testcenter-broadcasting-service:3000'
