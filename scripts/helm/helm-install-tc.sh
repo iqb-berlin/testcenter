6271032a1ec71ee299eefe15b7dde97bd14683cd
svwolter <sven.wolter@iqb.hu-berlin.de>
2025-06-05 14:22:20 +0200

    [helm] Revise 'Helm' installation script due to the use of Helm Chart Registries
    - change to latest stable of longhorn and traefik
    - tls_enabled now on default ON
    - seperate handling of TESTCENTER_CHART_VERSION, TRAEFIK_CHART_VERSION, TRAEFIK_CRDS_CHART_VERSION
    
    - longhorn always the chart version from upstream and uses this version deterministically (before the local chart was used)
    
    - traefik values is now pulled from upstream and edited with sed in-place
    - benefits: start from upstream defaults, no manual resync needed, only the deviations from defaults in `sed` commands in git, more deterministic as the base is pulled from upstream and not the local state of the traefik-values.yaml
    
    - routes dashboard to traefik.<BASE_DOMAIN>
    - add rerouting of entrypoint web to websecure, if tlsEnabled
    - disable telemetry
    - add tlsOptions like in docker-compose setup

--- scripts/helm/helm-install-tc.sh
+++ scripts/helm/helm-install-tc.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 declare TESTCENTER_VERSION="16.2.0"
-declare TRAEFIK_VERSION="v3.3.2"
-declare LONGHORN_VERSION="v1.7.2"
+declare TESTCENTER_CHART_VERSION="1.2.0"
+declare TRAEFIK_VERSION="v3.4.0"
+declare TRAEFIK_CHART_VERSION="35.4.0"
+declare TRAEFIK_CRDS_CHART_VERSION="1.8.1"
+declare LONGHORN_VERSION="v1.9.0"
 declare REQUIRED_PACKAGES=("kubectl version" "kubectl cluster-info" "helm version")
 
 declare LONGHORN_ENABLED=false
@@ -12,7 +15,7 @@ declare -A TRAEFIK_ENV_VARS
 TRAEFIK_ENV_VARS[TESTCENTER_BASE_DOMAIN]=testcenter.domain.tld
 TRAEFIK_ENV_VARS[HTTP_PORT]=80
 TRAEFIK_ENV_VARS[HTTPS_PORT]=443
-TRAEFIK_ENV_VARS[TLS_ENABLED]=false
+TRAEFIK_ENV_VARS[TLS_ENABLED]=true
 declare TRAEFIK_ENV_VAR_ORDER=(TESTCENTER_BASE_DOMAIN HTTP_PORT HTTPS_PORT TLS_ENABLED)
 
 declare -A TESTCENTER_ENV_VARS
@@ -49,8 +52,13 @@ install_longhorn() {
 
   if ! [[ ${continue} =~ ^[nN]$ ]]; then
     printf "Installing 'Longhorn' in the 'longhorn-system' namespace ...\n"
+    helm repo add longhorn https://charts.longhorn.io --force-update
 
-    if ! helm install longhorn ./longhorn --namespace longhorn-system --create-namespace; then
+    if ! helm install longhorn longhorn/longhorn \
+      --namespace longhorn-system \
+      --create-namespace \
+      --version ${LONGHORN_VERSION}; then
+
       printf "\n'Longhorn %s' installation failed.\n" ${LONGHORN_VERSION}
       read -rep "Do you want to continue anyway? [y/N] " -n 1 continue
 
@@ -74,17 +82,18 @@ install_traefik() {
 
   if ! [[ ${continue} =~ ^[nN]$ ]]; then
 
+    printf "Download Traefik default deployment configuration (traefik-values.yaml) for customization ...\n"
+    helm repo add traefik https://traefik.github.io/charts --force-update
+    helm show values traefik/traefik --version ${TRAEFIK_CHART_VERSION} > traefik-values.yaml
+    printf "Traefik default deployment configuration download done.\n\n"
+
     if ${LONGHORN_ENABLED}; then
-      printf "Configure Traefik 'custom-values' for 'longhorn persistent volumes' ...\n"
-      sed -i.bak "s|accessMode:.*|accessMode: ReadWriteMany|" \
-        custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-      sed -i.bak "s|storageClass:.*|storageClass: longhorn|" \
-        custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-      sed -i.bak "s|#fsGroup:|fsGroup:|" \
-        custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-      sed -i.bak "s|#fsGroupChangePolicy:|fsGroupChangePolicy:|" \
-        custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-      printf "Traefik 'custom-values' configuration for 'longhorn persistent volumes' done.\n\n"
+      printf "Configure Traefik for 'longhorn persistent volumes' ...\n"
+      sed -i.bak '/^persistence:$/{:persistence; $!N; /enabled: \(true\|false\)$/! b persistence; s//enabled: true/}'\
+        traefik-values.yaml && rm traefik-values.yaml.bak
+      sed -i.bak "s|accessMode:.*|accessMode: ReadWriteMany|" traefik-values.yaml && rm traefik-values.yaml.bak
+      sed -i.bak "s|storageClass:.*|storageClass: longhorn|" traefik-values.yaml && rm traefik-values.yaml.bak
+      printf "Traefik configuration for 'longhorn persistent volumes' done (q.v. traefik-values.yaml).\n\n"
     fi
 
     printf "Configure Traefik Ingress for Testcenter ...\n"
@@ -96,18 +105,73 @@ install_traefik() {
       TRAEFIK_ENV_VARS[${traefik_env_var_name}]=${traefik_env_var_value}
     done
 
-    sed -i.bak "s|httpPort: \&httpPort.*|httpPort: \&httpPort ${TRAEFIK_ENV_VARS[HTTP_PORT]}|" \
-      custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-    sed -i.bak "s|httpsPort: \&httpsPort.*|httpsPort: \&httpsPort ${TRAEFIK_ENV_VARS[HTTPS_PORT]}|" \
-      custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-    sed -i.bak "s|tlsEnabled: \&tlsEnabled.*|tlsEnabled: \&tlsEnabled ${TRAEFIK_ENV_VARS[TLS_ENABLED]}|" \
-      custom/traefik/custom-values.yaml && rm custom/traefik/custom-values.yaml.bak
-    printf "Traefik Ingress configuration for Testcenter done.\n\n"
+    # ingressRoute.dashboard
+    sed -i.bak "s|PathPrefix(\`/dashboard\`).*|Host(\`traefik.${TRAEFIK_ENV_VARS[TESTCENTER_BASE_DOMAIN]}\`)|" \
+      traefik-values.yaml && rm traefik-values.yaml.bak
+
+    # globalArguments
+    sed -i.bak "s|--global.checknewversion|--global.checknewversion=false|" \
+      traefik-values.yaml && rm traefik-values.yaml.bak
+    sed -i.bak "s|--global.sendanonymoususage|--global.sendanonymoususage=false|" \
+      traefik-values.yaml && rm traefik-values.yaml.bak
+
+    # ports
+    declare is_tls_enabled=${TRAEFIK_ENV_VARS[TLS_ENABLED]}
+
+    ## web
+    sed -i.bak "s|exposedPort: 80|exposedPort: ${TRAEFIK_ENV_VARS[HTTP_PORT]}|" \
+      traefik-values.yaml && rm traefik-values.yaml.bak
+    if ${is_tls_enabled}; then
+      sed -i.bak "s|entryPoint: {}|entryPoint:\n        to: websecure\n        scheme: https|" \
+        traefik-values.yaml && rm traefik-values.yaml.bak
+    fi
+
+    ## websecure
+    sed -i.bak "s|exposedPort: 443|exposedPort: ${TRAEFIK_ENV_VARS[HTTPS_PORT]}|" \
+      traefik-values.yaml && rm traefik-values.yaml.bak
+
+    ### tls
+    sed -i.bak '/^  websecure:$/{
+    :websecure
+    $!N
+    /tls:$/! b websecure
+    {
+    :tls
+    $!N
+    /enabled: \(true\|false\)$/! b tls
+    s//enabled: '"$is_tls_enabled"'/
+    }
+    }' traefik-values.yaml && rm traefik-values.yaml.bak
+
+    # tlsOptions
+    declare tls_options
+    tls_options="tlsOptions:\n"
+    tls_options+="  default:\n"
+    tls_options+="    sniStrict: false # set false to use self-signed certificates\n"
+    tls_options+="    cipherSuites:\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_RC4_128_SHA\n"
+    tls_options+="      - TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256\n"
+    tls_options+="      - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256\n"
+    tls_options+="      - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384\n"
+    tls_options+="      - TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256\n"
+    tls_options+="    minVersion: VersionTLS12\n"
+
+    if ${is_tls_enabled}; then
+      sed -i.bak "s|tlsOptions: {}|$tls_options|" traefik-values.yaml && rm traefik-values.yaml.bak
+    fi
+    printf "Traefik Ingress configuration for Testcenter done (q.v. traefik-values.yaml).\n\n"
 
     printf "Installing 'Traefik' Ingress Controller in the 'kube-system' namespace ...\n"
     printf -- "-> 'Install Traefik-CRDs' ...\n"
+    if ! helm install traefik-crds traefik/traefik-crds \
+      --namespace kube-system \
+      --version ${TRAEFIK_CRDS_CHART_VERSION}; then
 
-    if ! helm install traefik-crds ./traefik-crds --namespace kube-system; then
       printf "\n-> 'Traefik-CRDs' installation failed.\n"
       read -rep "Do you want to continue anyway? [y/N] " -n 1 continue
 
@@ -121,12 +185,11 @@ install_traefik() {
     fi
 
     printf "\n-> 'Install Traefik' ...\n"
-
-    if ! helm install traefik ./traefik \
+    if ! helm install traefik traefik/traefik \
       --namespace kube-system \
-      --values ./traefik/values.yaml \
-      --values ./traefik/custom-values.yaml \
-      --skip-crds; then
+      --values traefik-values.yaml \
+      --skip-crds \
+      --version ${TRAEFIK_CHART_VERSION}; then
 
       printf "\n-> 'Traefik %s' installation failed.\n" ${TRAEFIK_VERSION}
       read -rep "Do you want to continue anyway? [y/N] " -n 1 continue
@@ -154,35 +217,37 @@ install_testcenter() {
   read -rep "Do you want to install 'Testcenter ${TESTCENTER_VERSION}'? [Y/n] " -n 1 continue
 
   if ! [[ ${continue} =~ ^[nN]$ ]]; then
+    printf "Download Testcenter default deployment configuration (testcenter-values.yaml) for customization ...\n"
+    helm show values oci://registry-1.docker.io/iqbberlin/testcenter --version ${TESTCENTER_CHART_VERSION} > testcenter-values.yaml
+    printf "Testcenter default deployment configuration download done.\n\n"
 
     if ${LONGHORN_ENABLED}; then
-      printf "Configure Testcenter 'custom-values' for 'longhorn persistent volumes' ...\n"
+      printf "Configure Testcenter 'testcenter-values' for 'longhorn persistent volumes' ...\n"
       sed -i.bak "s|longhornEnabled:.*|longhornEnabled: true|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|cacheServerPvcStorageClassName:.*|cacheServerPvcStorageClassName: longhorn-single|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|cacheServerPvcAccessMode:.*|cacheServerPvcAccessMode: ReadWriteOnce|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|backendPvcStorageClassName:.*|backendPvcStorageClassName: longhorn|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|backendPvcAccessMode:.*|backendPvcAccessMode: ReadWriteMany|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|backendConfigPvcStorageClassName:.*|backendConfigPvcStorageClassName: longhorn|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|backendConfigPvcAccessMode:.*|backendConfigPvcAccessMode: ReadWriteMany|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|dbPvcStorageClassName:.*|dbPvcStorageClassName: longhorn-single|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
       sed -i.bak "s|dbPvcAccessMode:.*|dbPvcAccessMode: ReadWriteOnce|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
-      printf "Testcenter 'custom-values' configuration for 'longhorn persistent volumes' done.\n\n"
+        testcenter-values.yaml && rm testcenter-values.yaml.bak
+      printf "Testcenter 'testcenter-values' configuration for 'longhorn persistent volumes' done (q.v. testcenter-values.yaml).\n\n"
     fi
 
-    printf "Configure Testcenter 'custom-values' ...\n"
-
+    printf "Customize Testcenter default configuration ...\n"
     if ${TRAEFIK_ENABLED}; then
-      sed -i.bak "s|traefikEnabled:.*|traefikEnabled: true|" \
-        testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      sed -i.bak "s|traefikEnabled:.*|traefikEnabled: true|" testcenter-values.yaml && rm testcenter-values.yaml.bak
+
     else
       declare traefik_env_var_name
 
@@ -195,7 +260,6 @@ install_testcenter() {
     fi
 
     declare testcenter_env_var_name
-
     for testcenter_env_var_name in "${TESTCENTER_ENV_VAR_ORDER[@]}"; do
       declare testcenter_env_var_nameenv_var_value
       read -rep "${testcenter_env_var_name}: " -i "${TESTCENTER_ENV_VARS[${testcenter_env_var_name}]}" \
@@ -204,31 +268,30 @@ install_testcenter() {
     done
 
     sed -i.bak "s|baseDomain:.*|baseDomain: ${TRAEFIK_ENV_VARS[TESTCENTER_BASE_DOMAIN]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|httpPort:.*|httpPort: ${TRAEFIK_ENV_VARS[HTTP_PORT]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|httpsPort:.*|httpsPort: ${TRAEFIK_ENV_VARS[HTTPS_PORT]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|tlsEnabled:.*|tlsEnabled: ${TRAEFIK_ENV_VARS[TLS_ENABLED]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
 
     sed -i.bak "s|mysqlRootPassword:.*|mysqlRootPassword: ${TESTCENTER_ENV_VARS[MYSQL_ROOT_PASSWORD]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|mysqlUser: \&dbUser.*|mysqlUser: \&dbUser ${TESTCENTER_ENV_VARS[MYSQL_USER]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|mysqlPassword: \&dbUserPassword.*|mysqlPassword: \&dbUserPassword ${TESTCENTER_ENV_VARS[MYSQL_PASSWORD]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
     sed -i.bak "s|passwordSalt:.*|passwordSalt: ${TESTCENTER_ENV_VARS[MYSQL_SALT]}|" \
-      testcenter/custom-values.yaml && rm testcenter/custom-values.yaml.bak
-    printf "Testcenter 'custom-values' configuration for Traefik Ingress Controller done.\n\n"
+      testcenter-values.yaml && rm testcenter-values.yaml.bak
+    printf "Customization of Testcenter default configuration done (q.v. testcenter-values.yaml).\n\n"
 
     printf "Installing 'Testcenter' in the 'tc' namespace ...\n"
-
-    if ! helm install testcenter ./testcenter \
+    if ! helm install testcenter oci://registry-1.docker.io/iqbberlin/testcenter \
       --namespace tc \
       --create-namespace \
-      --values ./testcenter/values.yaml \
-      --values ./testcenter/custom-values.yaml; then
+      --values testcenter-values.yaml \
+      --version ${TESTCENTER_CHART_VERSION}; then
 
       printf "\n'Testcenter %s' installation failed.\n" ${TESTCENTER_VERSION}
       read -rep "Do you want to continue anyway? [y/N] " -n 1 continue
