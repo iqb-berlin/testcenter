905401f041d990727fc0695a217f5ee5c767e034
tobhu1981 <tobias.huste@hu-berlin.de>
2025-11-17 11:04:23 +0100

    [e2e] Improve stability of session-management: hot-return-mode
    Remove unnecessary waiting times for responses-complete,
    it is not necessary to give a response for a session entry
    in the response file.
    
    Use an interception: Make sure the session has been saved.
    
    Set test-isolation true and remove logouts
    
    Interception for response download for more stability
    
    Remove unnecessary testcode

--- e2e/src/e2e/Session-Management/hot-return-mode.cy.ts
+++ e2e/src/e2e/Session-Management/hot-return-mode.cy.ts
@@ -4,15 +4,13 @@ import {
   getResultFileRows,
   loginSuperAdmin,
   loginTestTaker,
-  logoutAdmin,
-  logoutTestTaker,
   openSampleWorkspace,
   probeBackendApi,
   resetBackendData,
   visitLoginPage
 } from '../utils';
 
-describe('Check hot-return mode functions', { testIsolation: false }, () => {
+describe('Check hot-return mode functions', { testIsolation: true }, () => {
   // TODO Testfälle bzgl. Ticket #315 erstellen
   before(() => {
     deleteDownloadsFolder();
@@ -27,55 +25,35 @@ describe('Check hot-return mode functions', { testIsolation: false }, () => {
 
   it('start first session', () => {
     loginTestTaker('hret1', '201', 'test-hot');
+    cy.intercept(new RegExp(`${Cypress.env('urls').backend}/test/\\d+/unit/CY-Unit.Sample-101/.*`)).as('waitUnitLoad');
     cy.get('[data-cy="unit-title"]')
       .contains('Aufgabe1');
-    getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
-      .click()
-      .should('be.checked');
-    cy.get('[data-cy="logo"]')
-      .click();
-    cy.log('end test');
-    cy.get('[data-cy="endTest-1"]')
-      .click();
-    cy.get('[data-cy="card-login-name"]')
-      .contains('hret1');
-    cy.get('[data-cy="logout"]')
-      .click();
+    //make sure the session has been added
+    cy.wait(['@waitUnitLoad']);
   });
 
-  it('second login does not create a new session', () => {
+  it('continue the test with login from first session', () => {
     loginTestTaker('hret1', '201', 'test-hot');
-    getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
-      .should('be.checked');
-    logoutTestTaker('hot');
+    cy.get('[data-cy="unit-title"]')
+      .contains('Aufgabe1');
   });
 
   it('start a second session', () => {
     loginTestTaker('hret2', '202', 'test-hot');
+    cy.intercept(new RegExp(`${Cypress.env('urls').backend}/test/\\d+/unit/CY-Unit.Sample-101/.*`)).as('waitUnitLoad');
     cy.get('[data-cy="unit-title"]')
       .contains('Aufgabe1');
-    getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
-      .click()
-      .should('be.checked');
-    cy.get('[data-cy="logo"]')
-      .click();
-    cy.log('end test');
-    cy.get('[data-cy="endTest-1"]')
-      .click();
-    cy.get('[data-cy="card-login-name"]')
-      .contains('hret2');
-    cy.get('[data-cy="logout"]')
-      .click();
+    //make sure the session has been added
+    cy.wait(['@waitUnitLoad']);
   });
 
-  it('second login does not create a new session', () => {
+  it('continue the test with login from second session', () => {
     loginTestTaker('hret2', '202', 'test-hot');
-    getFromIframe('[data-cy="TestController-radio1-Aufg1"]')
-      .should('be.checked');
-    logoutTestTaker('hot');
+    cy.get('[data-cy="unit-title"]')
+      .contains('Aufgabe1');
   });
 
-  it('generated file (responses, logs) exist in workspace with session group names', () => {
+  it('generated responses file exist in workspace with saved session-login', () => {
     loginSuperAdmin();
     openSampleWorkspace(1);
     cy.get('[data-cy="Ergebnisse/Antworten"]')
@@ -83,14 +61,13 @@ describe('Check hot-return mode functions', { testIsolation: false }, () => {
     cy.contains('SessionManagement Hot-Modes-Test Logins');
     cy.get('[data-cy="results-checkbox1"]')
       .click();
+    cy.intercept('GET', `${Cypress.env('urls').backend}/workspace/1/report/response?*`).as('waitForDownload');
     cy.get('[data-cy="download-responses"]')
       .click();
-    logoutAdmin();
-  });
-
-  it('session login must be saved in response file', () => {
+    cy.wait('@waitForDownload');
     getResultFileRows('responses')
       .then(responses => {
+        //checks if only two sessions were created
         expect(responses[1]).to.be.match(/\bhret1\b/);
         expect(responses[2]).to.be.match(/\bhret2\b/);
       });
