4e0d8d129aa445c3a7b2a295366d428486ff57d5
rhenck <richard.henck@iqb.hu-berlin.de>
2025-11-20 15:16:42 +0100

    [fe] Cleanup global styling
    Remove several styling commands and move some to their respective
    components.

--- frontend/src/app/app-root/app-root.component.ts
+++ frontend/src/app/app-root/app-root.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: `<div class="root-body">
+    template: `<div>
            <router-outlet></router-outlet>
          </div>
         `,
