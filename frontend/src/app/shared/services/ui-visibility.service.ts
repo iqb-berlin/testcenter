7a0decbc439846a6b25afe75cae613fc4298ff93
rhenck <richard.henck@iqb.hu-berlin.de>
2025-12-03 16:28:15 +0100

    [fe] Add explanatory code comment

--- frontend/src/app/shared/services/ui-visibility.service.ts
+++ frontend/src/app/shared/services/ui-visibility.service.ts
@@ -4,13 +4,14 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/* This service allows to hide UI elements like prompts and everything that may interfere with the test.
+   "ConfirmationUI" means all elements that may pop up. */
 export class UiVisibilityService {
   private showConfirmationUISubject = new BehaviorSubject<boolean>(true);
-  
+
   readonly showConfirmationUI$ = this.showConfirmationUISubject.asObservable();
 
   setShowConfirmationUI(show: boolean): void {
     this.showConfirmationUISubject.next(show);
   }
-
 }
