1bf6c400b1cf130947f1510681274645ebe2d3e7
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2025-12-04 08:36:08 +0100

    [fe] Add basic functionality
    - Click on buttons, adds the icon to the selection bar
    - Click on 'delete', deletes all icons

--- frontend/src/app/app-root/code-input/fab-form/fab-form.component.spec.ts
+++ frontend/src/app/app-root/code-input/fab-form/fab-form.component.spec.ts
@@ -17,7 +17,27 @@ describe('FabFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('creates', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('adds the right icon to select bar', () => {
+
+  });
+
+  it('does not overflow, when the max limit of selected icons is reached', () => {
+
+  });
+
+  it('deletes all icons, when pushing the delete button', () => {
+
+  });
+
+  it('sends the request automatically, when icon bar is full', () => {
+
+  });
+
+  it('gives feedback, when the code is wrong', () => {
+
   });
 });
--- frontend/src/app/app-root/code-input/fab-form/fab-form.component.ts
+++ frontend/src/app/app-root/code-input/fab-form/fab-form.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+interface GridIcon {
+  id: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-fab-form',
-  imports: [],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './fab-form.component.html',
   styleUrl: './fab-form.component.css',
 })
 export class FabFormComponent {
 
+  gridIcons: GridIcon[][] = [
+    [
+      { id: 'star', icon: 'star' },
+      { id: 'heart', icon: 'favorite' },
+      { id: 'square', icon: 'crop_square' }
+    ],
+    [
+      { id: 'home', icon: 'home' },
+      { id: 'smiley', icon: 'sentiment_satisfied' },
+      { id: 'cloud', icon: 'cloud' }
+    ],
+    [
+      { id: 'flower', icon: 'filter_vintage' },
+      { id: 'sun', icon: 'wb_sunny' },
+      { id: 'triangle', icon: 'change_history' }
+    ]
+  ];
+
+  selectedIcons = signal<(GridIcon | null)[]>(Array(5).fill(null));
+  nextIcon = computed(() => this.selectedIcons().indexOf(null))
+
+  clearSelection() {
+    this.selectedIcons.set(Array(5).fill(null));
+  }
+
+  onIconSelect(arg: GridIcon) {
+    if (this.nextIcon() >= 0) {
+      this.selectedIcons.update(icons => {
+        const newIcons = [...icons];
+        newIcons[this.nextIcon()] = arg;
+        return newIcons;
+      });
+    }
+  }
 }
