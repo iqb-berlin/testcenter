a2e80477dc18af27949b910f274ba2785ba08e5d
Benjamin Becker <b.becker@iqb.hu-berlin.de>
2025-12-10 15:12:20 +0100

    [fe] Separate admin login from user login
    For now, code and template are duplicated. Leaner solution will follow.

---
+++ frontend/src/app/app-root/admin-login/admin-login.component.ts
@@ -0,0 +1,136 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MainDataService, UserAgentService } from '../../shared/shared.module';
+import { AuthData } from '../../app.interfaces';
+import { BackendService } from '../../backend.service';
+
+@Component({
+    templateUrl: './admin-login.component.html',
+    styles: [
+        '.mat-mdc-form-field {display: block}',
+        '.mat-mdc-card {width: 400px;}',
+        '.version-label {position: fixed; bottom: 0; right: 0; background: rgba(255,255,255, 0.3); padding: 0.5em 1em}',
+        '.version-label a {color:black; text-decoration: none; font-size: large}'
+    ],
+    standalone: false
+})
+
+export class AdminLoginComponent implements OnInit, OnDestroy {
+  static oldLoginName = '';
+  private routingSubscription: Subscription | null = null;
+  returnTo = '';
+  problemText = '';
+  problemLevel: 'error' | 'warning' = 'error';
+  problemCode = 0;
+  showPassword = false;
+  unsupportedBrowser: [string, string] | [] = [];
+
+  loginForm = new FormGroup({
+    name: new FormControl(AdminLoginComponent.oldLoginName, [Validators.required, Validators.minLength(3)]),
+    pw: new FormControl('')
+  });
+
+  constructor(
+    public mainDataService: MainDataService,
+    private backendService: BackendService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.mainDataService.appSubTitle$.next('Bitte anmelden');
+    this.routingSubscription = this.route.params
+      .subscribe(params => { this.returnTo = params.returnTo; });
+    this.checkBrowser();
+  }
+
+  login(loginType: 'admin' | 'login' = 'login'): void {
+    const loginData = this.loginForm.value;
+    if (!loginData.name) {
+      return;
+    }
+    AdminLoginComponent.oldLoginName = loginData.name;
+    this.problemText = '';
+    this.problemCode = 0;
+    this.backendService.login(loginType, loginData.name, loginData.pw ?? '').subscribe({
+      next: authData => {
+        const authDataTyped = authData as AuthData;
+        this.mainDataService.setAuthData(authDataTyped);
+        if (this.returnTo) {
+          this.router.navigateByUrl(this.returnTo).then(navOk => {
+            if (!navOk) {
+              this.router.navigate(['/r']);
+            }
+          });
+        } else if (!authData.flags.includes('codeRequired') && loginType === 'login') {
+          if (authData.claims.test && authData.claims.test.length === 1 && Object.keys(authData.claims).length === 1) {
+            this.backendService.startTest(authData.claims.test[0].id).subscribe({
+              next: testId => {
+                this.router.navigate(['/t', testId]);
+              },
+              error: () => {
+                this.router.navigate(['/r/starter']);
+              }
+            });
+          } else if (authData.claims.sysCheck && authData.claims.sysCheck.length === 1 && Object.keys(authData.claims).length === 1) {
+            this.router.navigate(['/check', authData.claims.sysCheck[0].workspaceId, authData.claims.sysCheck[0].id]);
+          } else {
+            this.router.navigate(['/r/starter']);
+          }
+        } else {
+          this.router.navigate(['/r']);
+        }
+      },
+      error: error => {
+        this.problemCode = error.code;
+        if (error.code === 400) {
+          this.problemText = 'Anmeldedaten sind nicht gültig. Bitte noch einmal versuchen!';
+        } else if (error.code === 401) {
+          this.problemText = 'Anmeldung abgelehnt. Anmeldedaten sind noch nicht freigeben.';
+        } else if (error.code === 204) {
+          this.problemText = 'Anmeldedaten sind gültig, aber es sind keine Arbeitsbereiche oder Tests freigegeben.';
+        } else if (error.code === 410) {
+          this.problemText = 'Anmeldedaten sind abgelaufen';
+        } else if (error.code === 429) {
+          this.problemText = 'Zu viele Fehlversuche! Probieren Sie es zu einem späteren Zeitpunkt noch einmal.';
+        } else {
+          this.problemText = 'Problem bei der Anmeldung.';
+          throw error;
+        }
+        this.problemLevel = 'error';
+        this.loginForm.reset();
+      }
+    });
+  }
+
+  checkCapsLock(event: KeyboardEvent): void {
+    // some newer edge versions does fire a keyup event when clicking into the textfield, which does not
+    // have getModifierState TODO find the route cause of this instead of workaround
+    if (typeof event.getModifierState !== 'function') return;
+    if (event.getModifierState('CapsLock')) {
+      this.problemText = 'Feststelltaste ist aktiviert!';
+      this.problemLevel = 'warning';
+    }
+  }
+
+  clearWarning(): void {
+    this.problemText = '';
+    this.problemLevel = 'error';
+    this.problemCode = 0;
+  }
+
+  private checkBrowser() {
+    const ua = UserAgentService.resolveUserAgent();
+    if (!UserAgentService.userAgentMatches(ua)) {
+      this.unsupportedBrowser = [ua.family, ua.version];
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.routingSubscription !== null) {
+      this.routingSubscription.unsubscribe();
+    }
+  }
+}
