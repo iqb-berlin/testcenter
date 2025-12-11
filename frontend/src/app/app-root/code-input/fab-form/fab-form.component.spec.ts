dc926184394ce658a85abd7989cbc4b878ca32b3
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2025-12-11 12:16:31 +0100

    Add test 2

--- frontend/src/app/app-root/code-input/fab-form/fab-form.component.spec.ts
+++ frontend/src/app/app-root/code-input/fab-form/fab-form.component.spec.ts
@@ -37,7 +37,22 @@ describe('FabFormComponent', () => {
   });
 
   it('deletes all icons, when pushing the delete button', () => {
+    const { debugElement } = fixture;
+    // Arrange
+    const [gridIcon0, gridIcon1] = debugElement.queryAll(By.css('[data-gridPos]'));
+    const getNumberOfEmptyIcons = () => debugElement.queryAll(By.css('.placeholder')).length;
 
+    gridIcon0.triggerEventHandler('click', null);
+    gridIcon1.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(getNumberOfEmptyIcons()).toBe(3);
+
+    // Act
+    const deleteButton = debugElement.query(By.css('.delete-button'));
+    deleteButton.triggerEventHandler('click', null);
+
+    // Assert
+    expect(getNumberOfEmptyIcons()).toBe(5);
   });
 
   it('sends the request automatically, when icon bar is full', () => {
