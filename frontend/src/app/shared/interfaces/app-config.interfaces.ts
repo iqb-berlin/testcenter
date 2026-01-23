27fb2bb33319abf5f82ab7b8dac0419e7766fe47
rhenck <richard.henck@iqb.hu-berlin.de>
2026-01-23 18:49:24 +0100

    [fe][theming] Remove old color settings
    The background and box colors are now part of the theme and can be
    customized there.

--- frontend/src/app/shared/interfaces/app-config.interfaces.ts
+++ frontend/src/app/shared/interfaces/app-config.interfaces.ts
@@ -3,8 +3,6 @@ import { KeyValuePairs } from '../../app.interfaces';
 export interface AppSettingsObject {
   appTitle: string,
   mainLogo: string,
-  backgroundBody: string,
-  backgroundBox: string,
   introHtml: string,
   legalNoticeHtml: string,
   globalWarningText: string,
@@ -30,6 +28,3 @@ export interface SysConfig {
 }
 
 export const DEFAULT_LOGO = 'assets/IQB-LogoA.png';
-export const DEFAULT_BACKGROUND_BODY =
-  '#003333 linear-gradient(to bottom, #003333, #045659, #0d7b84, #1aa2b2, #2acae5)';
-export const DEFAULT_BACKGROUND_BOX = 'lightgray';
