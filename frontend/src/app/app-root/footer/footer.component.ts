ce06c2f6348d58ec469053c9f96bb6cb4d62eeba
rhenck <richard.henck@iqb.hu-berlin.de>
2026-01-23 23:23:11 +0100

    [fe][uiux] Remove unwanted elements from login page
    Content that we want to keep, like links have been moved onto the footer
    for now.

--- frontend/src/app/app-root/footer/footer.component.ts
+++ frontend/src/app/app-root/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { MainDataService } from '../../shared/services/maindata/maindata.service';
+import { MatButton } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tc-footer',
   imports: [
-    NgIf
+    NgIf,
+    MatButton,
+    RouterLink
   ],
   template: `
     <footer>
@@ -14,16 +18,27 @@ import { MainDataService } from '../../shared/services/maindata/maindata.service
         <span *ngIf="mainDataService.isTestingMode" style="color:red">Testmode!</span>
         IQB-Testcenter Version {{mainDataService.appConfig?.version}}
       </div>
+
+      <button matButton [routerLink]="['/r/admin-login']">Admin-Bereich</button>
+      <button matButton *ngIf="this.mainDataService.sysCheckAvailableForAll" data-cy="general-sys-check"
+         [routerLink]="['/r/check-starter']">
+        System-Check
+      </button>
+      <button matButton [routerLink]="['/legal-notice']">Impressum/Datenschutz</button>
+
     </footer>
   `,
   styles: `
     footer {
       height: 24px;
+      padding: 16px;
       background: var(--theme-Gray-05, #F4F2F2);
+      display: flex;
+      flex-direction: row;
     }
   `
 })
 export class FooterComponent {
-  constructor(public mainDataService: MainDataService) {
+  constructor(public mainDataService: MainDataService, private router: Router) {
   }
 }
