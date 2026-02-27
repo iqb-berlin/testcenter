9f4dd3f569c4de9efb56f87f4bc2763022d68040
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-02-27 12:36:12 +0100

    Join 17.6.0 to next.sql

--- scripts/database/patches.d/next.sql
+++ scripts/database/patches.d/next.sql
@@ -4,3 +4,9 @@ alter table login_session_groups
 alter table login_session_groups
   modify column last_modified timestamp not null;
 
+-- Optimize polling for commands
+CREATE INDEX `idx_test_commands_execution` ON `test_commands` (`test_id`, `executed`, `timestamp`);
+
+-- Optimize review sorting and filtering
+CREATE INDEX `idx_test_reviews_sorting` ON `test_reviews` (`booklet_id`, `person_id`, `reviewtime`);
+CREATE INDEX `idx_unit_reviews_sorting` ON `unit_reviews` (`test_id`, `unit_name`, `person_id`, `reviewtime`);
