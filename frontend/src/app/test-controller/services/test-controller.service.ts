de287cae50b2180a149064999c1e9101f2a7f27d
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-04-28 18:38:18 +0200

    Refactor blob string to source code

--- frontend/src/app/test-controller/services/test-controller.service.ts
+++ frontend/src/app/test-controller/services/test-controller.service.ts
@@ -503,25 +503,33 @@ export class TestControllerService {
     this.finishTimer();
   }
 
-  // TODO import the webworker from a seperate file. At time of implementing, some SCP problems occured
   private createTicker(): Observable<number> {
     if (typeof Worker !== 'undefined') {
-      const workerCode = `
-        let timer;
+      // Stringified via .toString() and run inside a Blob worker. Must be self-contained:
+      // no closures, no imports, no references to outer-scope identifiers.
+      const tickerWorker = (): void => {
+        let timer: ReturnType<typeof setInterval>;
         let secondsPassed = 0;
-        self.onmessage = function(message) {
+        self.onmessage = (message: MessageEvent<'on' | 'off'>) => {
+          console.log('webworker')
           switch (message.data) {
             case 'on':
               postMessage(secondsPassed++);
               timer = setInterval(() => postMessage(secondsPassed++), 1000);
-              console.log('timeTicker from webworker used');
               break;
             case 'off':
               clearInterval(timer);
+              break;
+            default:
           }
-        };  
-      `;
-      const blob = new Blob([workerCode], { type: 'application/javascript' });
+        };
+      };
+      const blob = new Blob(
+        // Take the function, convert it to text, wrap it as an expression, and generate code that calls it inside the worker.
+        // [tickerWorker()] would not work, as it would execute the function and send its return value to the worker, instead of the source code of the function
+        [`(${tickerWorker.toString()})();`],
+        { type: 'application/javascript' }
+      );
       const workerTimer = new Worker(URL.createObjectURL(blob));
       return new Observable(subscriber => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
