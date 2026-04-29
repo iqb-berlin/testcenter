f299e8d419cb1a07a615a7bb964b62f600380369
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-04-29 09:50:57 +0200

    Refactor worker code
    - Delete self, as not needed for the window object
    - use terminate() and revokeObjectUrl() on unsubscribe(), to properly clean up the resources. Before was memory leak after every call.
    - Remove ts-ignore
    - Remove one level of indentation

--- frontend/src/app/test-controller/util/worker.util.ts
+++ frontend/src/app/test-controller/util/worker.util.ts
@@ -1,48 +1,49 @@
 import { interval, Observable } from 'rxjs';
 
 export function createTicker(): Observable<number> {
-  if (typeof Worker !== 'undefined') {
+  if (typeof Worker === 'undefined') {
+    return interval(1000);
+  }
   // Stringified via .toString() and run inside a Blob worker. Must be self-contained:
   // no closures, no imports, no references to outer-scope identifiers.
-    const tickerWorker = (): void => {
-      let timer: ReturnType<typeof setInterval>;
-      let secondsPassed = 0;
-      self.onmessage = (message: MessageEvent<'on' | 'off'>) => {
-        console.log('webworker');
-        switch (message.data) {
-        case 'on':
-          postMessage(secondsPassed++);
-          timer = setInterval(() => postMessage(secondsPassed++), 1000);
-          break;
-        case 'off':
-          clearInterval(timer);
-          break;
-        default:
-        }
-      };
+  const tickerWorker = (): void => {
+    let timer: ReturnType<typeof setInterval>;
+    let secondsPassed = 0;
+    onmessage = (message: MessageEvent<'on' | 'off'>) => {
+      console.log('webworker');
+      switch (message.data) {
+      case 'on':
+        postMessage(secondsPassed++);
+        timer = setInterval(() => postMessage(secondsPassed++), 1000);
+        break;
+      case 'off':
+        clearInterval(timer);
+        break;
+      default:
+      }
     };
-    const blob = new Blob(
-    // Take the function, convert it to text, wrap it as an expression, and generate code that calls it inside the worker.
-    // [tickerWorker()] would not work, as it would execute the function and send its return value to the worker, instead of the source code of the function
-      [`(${tickerWorker.toString()})();`],
-      { type: 'application/javascript' }
-    );
-    const workerTimer = new Worker(URL.createObjectURL(blob));
-    return new Observable(subscriber => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-      const eventHandler = event => {
-        subscriber.next(event.data);
-      };
+  };
+  const blob = new Blob(
+  // Take the function, convert it to text, wrap it as an expression, and generate code that calls it inside the worker.
+  // [tickerWorker()] would not work, as it would execute the function and send its return value to the worker, instead of the source code of the function
+    [`(${tickerWorker.toString()})();`],
+    { type: 'application/javascript' }
+  );
+  const blobUrl = URL.createObjectURL(blob);
+  const workerTimer = new Worker(blobUrl);
+  return new Observable(subscriber => {
+    const eventHandler = (event: MessageEvent<number>) => {
+      subscriber.next(event.data);
+    };
 
-      workerTimer.addEventListener('message', eventHandler);
-      workerTimer.postMessage('on');
+    workerTimer.addEventListener('message', eventHandler);
+    workerTimer.postMessage('on');
 
-      return function unsubscribe() {
-        workerTimer.postMessage('off');
-        workerTimer.removeEventListener('message', eventHandler);
-      };
-    });
-  }
-  return interval(1000);
+    return function unsubscribe() {
+      workerTimer.postMessage('off');
+      workerTimer.removeEventListener('message', eventHandler);
+      workerTimer.terminate();
+      URL.revokeObjectURL(blobUrl);
+    };
+  });
 }
