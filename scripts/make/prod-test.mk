ae7b22205768e570cfab79db15d44072786572b4
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-05-11 18:07:42 +0200

    Add $(service) to prod-test-build
    Better ergonomics

--- scripts/make/prod-test.mk
+++ scripts/make/prod-test.mk
@@ -3,7 +3,8 @@ TC_BASE_DIR := $(shell git rev-parse --show-toplevel)
 include $(TC_BASE_DIR)/.env.prod
 
 ## prevents collisions of make target names with possible file names
-.PHONY: prod-test-registry-login prod-test-registry-logout prod-test-build prod-test-up prod-test-down prod-test-logs
+.PHONY: prod-test-registry-login prod-test-registry-logout prod-test-build prod-test-up prod-test-down prod-test-logs \
+        _prod-test-build-backend _prod-test-build-frontend _prod-test-build-broadcaster _prod-test-build-file-server _prod-test-build-e2e
 
 ## disables printing the recipe of a make target before executing it
 .SILENT: prod-test-registry-login prod-test-registry-logout
@@ -17,7 +18,20 @@ prod-test-registry-logout:
 	if test $(DOCKER_HUB_PROXY); then docker logout $(DOCKER_HUB_PROXY); fi
 
 ## Build production and e2e test images
+# Param (optional): service - Build only the specified service, e.g. `make prod-test-build service=backend`
+#                             Allowed: backend, frontend, broadcaster, file-server, e2e
 prod-test-build: prod-test-registry-login
+ifeq ($(service),)
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-backend
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-frontend
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-broadcaster
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-file-server
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-e2e
+else
+	$(MAKE) -f $(TC_BASE_DIR)/scripts/make/prod-test.mk _prod-test-build-$(service)
+endif
+
+_prod-test-build-backend:
 	cd $(TC_BASE_DIR) &&\
 		docker build\
 				--progress plain\
@@ -26,6 +40,8 @@ prod-test-build: prod-test-registry-login
 				--file $(TC_BASE_DIR)/backend/Dockerfile\
 				--tag $(DOCKER_HUB_PROXY)iqbberlin/testcenter-backend:e2e\
 			.
+
+_prod-test-build-frontend:
 	cd $(TC_BASE_DIR) &&\
 		docker build\
 				--progress plain\
@@ -34,6 +50,8 @@ prod-test-build: prod-test-registry-login
 				--file $(TC_BASE_DIR)/frontend/Dockerfile\
 				--tag $(DOCKER_HUB_PROXY)iqbberlin/testcenter-frontend:e2e\
 			.
+
+_prod-test-build-broadcaster:
 	cd $(TC_BASE_DIR) &&\
 		docker build\
 				--progress plain\
@@ -42,6 +60,8 @@ prod-test-build: prod-test-registry-login
 				--file $(TC_BASE_DIR)/broadcaster/Dockerfile\
 				--tag $(DOCKER_HUB_PROXY)iqbberlin/testcenter-broadcaster:e2e\
 			.
+
+_prod-test-build-file-server:
 	cd $(TC_BASE_DIR) &&\
 		docker build\
 				--progress plain\
@@ -50,6 +70,8 @@ prod-test-build: prod-test-registry-login
 				--file $(TC_BASE_DIR)/file-server/Dockerfile\
 				--tag $(DOCKER_HUB_PROXY)iqbberlin/testcenter-file-server:e2e\
 			.
+
+_prod-test-build-e2e:
 	cd $(TC_BASE_DIR) &&\
 		docker build\
 				--progress plain\
@@ -85,4 +107,4 @@ prod-test-logs:
 			--env-file $(TC_BASE_DIR)/.env.prod\
 			--file $(TC_BASE_DIR)/docker-compose.yml\
 			--file $(TC_BASE_DIR)/docker-compose.prod.tls.yml\
-		logs -f $(SERVICE)
+		logs -f $(service)
