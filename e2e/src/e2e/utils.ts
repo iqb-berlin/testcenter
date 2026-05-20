10b02f309a40a2a7e25949419460519ce3a29690
tobhu1981 <tobias.huste@hu-berlin.de>
2026-05-20 11:48:13 +0200

    [e2e] Refactor utils
    Verify that the login page is reached after every logout.
    
    Removing Duplicate Queries
    
    No return of the CY value if no further CY command is chained in the sequence.

--- e2e/src/e2e/utils.ts
+++ e2e/src/e2e/utils.ts
@@ -156,7 +156,6 @@ export const logoutAdmin = () => cy.url()
       cy.url()
         .should('eq', `${Cypress.config().baseUrl}/#/r/starter`);
       logout();
-      cy.get('[data-cy="login-admin-form"]');
     }
     cy.get('[data-cy="login-admin-form"]');
   });
@@ -175,15 +174,13 @@ export const logoutFromRunningTestWithConfirmation = (): Chainable => {
       cy.get('[data-cy="logo"]')
         .click();
     }
-
     cy.get('[data-cy="endTest-1"]').click();
     cy.wait('@waitForGetSession');
     cy.url().should('eq', `${baseUrl}/#/r/starter`);
     cy.contains('Übersicht').should('be.visible');
     logout();
     cy.wait('@waitForDeleteSession');
-
-    return cy.get('[data-cy="login-admin-form"]');
+    cy.get('[data-cy="login-admin-form"]');
   });
 };
 
@@ -204,8 +201,7 @@ export const logoutFromTestNoConfirmation = (): Chainable => {
     cy.contains('Übersicht').should('be.visible');
     logout();
     cy.wait('@waitForDeleteSession');
-
-    return cy.get('[data-cy="login-admin-form"]');
+    cy.get('[data-cy="login-admin-form"]');
   });
 };
 
