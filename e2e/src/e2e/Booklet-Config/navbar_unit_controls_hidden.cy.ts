db65618e5502e2f5e0695f939b4d2ce3c0fd90a9
tobhu1981 <tobias.huste@hu-berlin.de>
2026-07-08 12:55:31 +0200

    [e2e] Fix: Wait for page load before asserting element absence and fix selectors

--- e2e/src/e2e/Booklet-Config/navbar_unit_controls_hidden.cy.ts
+++ e2e/src/e2e/Booklet-Config/navbar_unit_controls_hidden.cy.ts
@@ -21,15 +21,17 @@ describe('check parameter: navbar_unit_controls_hidden', { testIsolation: true }
     loginTestTaker('Bklt_Config-32', '123');
     cy.get('[data-cy="unit-navigation-forward"]')
       .should('be.visible');
-    cy.get('[data-cy="unit-nav-label-container"]')
+    cy.get('[data-cy="unit-navigation-label"]')
       .should('be.visible');
   });
 
   it('TRUE', () => {
     loginTestTaker('Bklt_Config-33', '123');
+    cy.get('[data-cy="unit-title"]')
+      .contains('Aufgabe1');
     cy.get('[data-cy="unit-navigation-forward"]')
       .should('not.exist');
-    cy.get('[data-cy="unit-nav-label-container"]')
+    cy.get('[data-cy="unit-navigation-label"]')
       .should('be.visible');
   });
 });
