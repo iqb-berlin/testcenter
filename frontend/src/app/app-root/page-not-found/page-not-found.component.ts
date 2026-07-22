2db0008554340f16a2f971a557cb0b8797539d95
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-07-22 15:12:46 +0200

    [fe] (refac) Rename RouterDispatcher to PageNotFound
    This shows the actual behavior of the component, which shows page not found. The real dispatching happens in the RouteDispatcherActivateGuard.

---
+++ frontend/src/app/app-root/page-not-found/page-not-found.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router, RouterState, RouterStateSnapshot } from '@angular/router';
+
+@Component({
+  template: `
+    <div class="flex-row" [style.justify-content]="'center'">
+      <mat-card appearance="outlined">
+        <mat-card-header>
+          <mat-card-title>Diese Seite wurde nicht gefunden.</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>{{url}}</mat-card-content>
+        <mat-card-actions>
+          <button [routerLink]="['/']" mat-raised-button>Zur Startseite</button>
+        </mat-card-actions>
+      </mat-card>
+    </div>
+  `,
+  styles: [
+    '.mat-mdc-card {width: 400px; margin-top: 80px;}'
+  ],
+  standalone: false
+})
+
+export class PageNotFoundComponent {
+  url = '';
+
+  constructor(
+    private router: Router
+  ) {
+    const state: RouterState = router.routerState;
+    const snapshot: RouterStateSnapshot = state.snapshot;
+    this.url = snapshot.url;
+  }
+}
