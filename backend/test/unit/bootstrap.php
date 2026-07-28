8abd671959b85908786bb4649a7096335b4cb9c2
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2026-07-28 22:18:23 +0200

    [be] Remove use of config.ini in unit tests
    Remove orphaned use of readConfigIni(). Unit tests still work with readFromEnvironment()

--- backend/test/unit/bootstrap.php
+++ backend/test/unit/bootstrap.php
@@ -4,11 +4,8 @@ define('ROOT_DIR', realpath(__DIR__ . '/../../..'));
 set_include_path(realpath(__DIR__ . '/..'));
 require_once ROOT_DIR . "/backend/vendor/autoload.php";
 
-try {
-  // todo kann das weg?
-  SystemConfig::readConfigIni();
-} catch (Exception $exception) {
-  SystemConfig::readEnvironment();
-}
+SystemConfig::readEnvironment();
+SystemConfig::$bruteForceProtection_sessions = [];
+SystemConfig::$server_key = 'Secret';
 
 SystemConfig::$debug_allowExternalXmlSchema = false;
