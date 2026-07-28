iqb-berlin/testcenter

commit a27233e00831d3dbdccb772c362334f0012d4346

[be] Create full.sql on every initialize
full.sql is useful for llms to see current shape of database. To keep it updated, it is now newly created at every initialize.

backend/initialize.php
@@ -125,6 +125,8 @@ try {
   $initDAO->setDBSchemaVersion($systemVersion);
   CLI::success("DB passed integrity check.");
 
+  $initDAO->writeFullSchema(ROOT_DIR . '/scripts/database/full.sql');
+
   CLI::h2("Workspaces");
 
   if (!file_exists(DATA_DIR)) {

backend/src/dao/InitDAO.class.php
@@ -417,6 +417,15 @@ class InitDAO extends SessionDAO {
     return $report;
   }
 
+  public function writeFullSchema(string $path): void {
+    $schema = '-- IQB-Testcenter DB --';
+    foreach ($this::tables as $table) {
+      $schema .= "\n\n" . $this->_("SHOW CREATE TABLE $table")['Create Table'] . ';';
+      $schema .= "\nTRUNCATE $table; -- to reset auto-increment";
+    }
+    file_put_contents($path, $schema);
+  }
+
   public function createSampleMetaData(): void {
     $this->setMeta('appConfig', 'aKey', 'newValue');
   }

backend/src/helper/TestEnvironment.class.php
@@ -154,12 +154,7 @@ class TestEnvironment {
       throw new Exception(implode("; ", $errors));
     }
 
-    $scheme = '-- IQB-Testcenter DB --';
-    foreach ($initDAO::tables as $table) {
-      $scheme .= "\n\n" . $initDAO->_("show create table $table")['Create Table'] . ";";
-      $scheme .= "\n" . "truncate $table; -- to reset auto-increment";
-    }
-    file_put_contents(ROOT_DIR . '/scripts/database/full.sql', $scheme);
+    $initDAO->writeFullSchema(ROOT_DIR . '/scripts/database/full.sql');
   }
 
   private static function rollback(): void {

commit 57453ce7edaf88d5ac311979ed6fa57024eb44ad

[be] Fix broken initialize tests for backend
Was introduced with writeFullSchema in a27233e00831d3dbdccb772c362334f0012d4346, which clashed with building partly constructed tables in the initialize/general scripts.

Check for completeness now makes sure that full.sql is only build, if all current tables exist.

backend/initialize.php
@@ -115,7 +115,10 @@ try {
   $initDAO->setDBSchemaVersion($systemVersion);
   CLI::success("DB passed integrity check.");
 
-  $initDAO->writeFullSchema(ROOT_DIR . '/scripts/database/full.sql');
+  // tables is 'complete', if the current database has all tables, declared in self::tables; not the case for initialize/general scripts that test incomplete table states
+  if ($newDbStatus['tables'] === 'complete') {
+    $initDAO->writeFullSchema(ROOT_DIR . '/scripts/database/full.sql');
+  }
 
   CLI::h2("Workspaces");
 

backend/test/initialization/tests/general/re-initialize.sh
@@ -15,7 +15,7 @@ source backend/test/initialization/functions/functions.sh
   expect_init_script_ok
   expect_data_dir_equals sample_content_present
   expect_table_to_have_rows files 11
-  expect_table_to_have_rows logins 15
+  expect_table_to_have_rows logins 16
 
   echo_h2 "Delete a testtakers-file manually and sync the DB when initializing again. Files should be re-imported"
 